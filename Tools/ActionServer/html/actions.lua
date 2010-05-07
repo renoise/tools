19dff0ff80ad8d322a4b1a0c3797e87113be9ed8
marvintjon <marvintjon@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-05-07 23:20:17 +0000

    Handle incoming Chunked Transfer Encoding Header parsed into table Minor fixes

--- Tools/ActionServer/html/actions.lua
+++ Tools/ActionServer/html/actions.lua
@@ -1,4 +1,4 @@
-~{do L:header("Cache-Control", "private, max-age: 3600") end}
+~{do L:set_header("Cache-Control", "private, max-age: 3600") end}
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" dir="ltr">
