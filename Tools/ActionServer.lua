6cb18b053e974033e31477c1f9bf86b0a98fe017
marvintjon <marvintjon@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-05-07 20:47:17 +0000

    1) Added Menu Options: Start/Stop/Config 2) Set INADDR_ANY (0.0.0.0) as default bind address 3) Moved utility functions into Util class 4) Fixed HTML pages so they work with INADDR_ANY

--- Tools/ActionServer.lua
+++ Tools/ActionServer.lua
@@ -7,16 +7,26 @@ manifest.api_version = 0.3
 manifest.author = "bantai [marvin@renoise.com]"
 manifest.description = "Exposes Renoise functions through networking"
 
--- require "remdebug.engine"
-
-manifest.actions = {}
-manifest.actions[#manifest.actions + 1] = {
-  name = "MainMenu:Tools:ActionServer",
-  description = "ActionServer",
+manifest.actions = table.create()
+manifest.actions:insert{
+  name = "MainMenu:Tools:ActionServer:Start",
+  description = "Start or restart the server",
   invoke = function()
---    remdebug.engine.start() -- debugger will connect and initially break here
     start_server()
---    remdebug.engine.stop()
+  end
+}
+manifest.actions:insert{
+  name = "MainMenu:Tools:ActionServer:Stop",
+  description = "Stop the server",
+  invoke = function()
+    stop_server()
+  end
+}
+manifest.actions:insert{
+  name = "MainMenu:Tools:ActionServer:Configure",
+  description = "Edit server preferences",
+  invoke = function()
+    configure_server()
   end
 }
 
@@ -46,33 +56,8 @@ local log = Log(Log.ALL)
 
 local expand = require "ActionServer.Expand"
 
-local echo_server = nil
+local action_server = nil
 local errors = {}
-local action_names = {}
-
-local message = {
-  boolean_value = nil,
-  int_value = nil,
-  
-  value_min_scaling = 0.0,
-  value_max_scaling = 1.0,
-  
-  is_trigger = function() return true end,
-  is_switch = function() return false end,
-  is_rel_value = function() return false end,
-  is_abs_value = function() return false end
-}
-
-for k,v in pairs(available_actions()) do
-  -- do not include window/dialog actions in unit tests...
-  if not string.find(v, "Window") and
-     not string.find(v, "Dialog") and
-     -- also Sequence muting, or tests will take days
-     not string.find(v, "Sequence XX") and
-     not string.find(v, "Seq. XX") then
-    action_names[#action_names + 1] = v
-  end
-end
 
 --log:info("number of actions;" .. #action_names)
 --log:info("action[1];" .. action_names[1])
@@ -81,47 +66,32 @@ end
 --  Util functions
 -------------------------------------------
 
-local function song()
+class 'Util'
+
+function Util:song()
   return renoise.song()
 end
 
-local function find_action(action_name)
-  if table.find(action_names,action_name) then
-    invoke_action(action_name, message)
-  else
-    log:warn("Action not found: " .. action_name)
-  end
---[[
-  if action_names[action_name] then
-    log:info("Executing: " .. action_name)
-    invoke_action(action_name, message)
-  else
-    log:warn("Action not found: " .. action_name)
-  end
-]]--
-end
-
-local function split_lines(str)
+function Util:split_lines(str)
   local t = {}
   local function helper(line) table.insert(t, line) return "" end
   helper((str:gsub("(.-)\r?\n", helper)))
   return t
 end
 
-local function parse_message(m)
+function Util:parse_message(m)
   local header = ""
   local body = ""
-  local lines = split_lines(m)
+  local lines = Util:split_lines(m)
   local s = false
   for _,l in pairs(lines) do
     if s then body=body..l.."\r\n" else header=header..l.."\r\n" end
     if l:match("^$") then s = true end
   end
-  rprint(lines)
   return header, body
 end
 
-local function read_file(file_path)
+function Util:read_file(file_path)
   local file_ref,err = io.open(file_path,"r")
   if not err then
     local result=file_ref:read("*all")
@@ -133,34 +103,33 @@ local function read_file(file_path)
 end
 
 -- Compute the difference in seconds between local time and UTC.
-local function get_timezone()
+function Util:get_timezone()
   local now = os.time()
   return os.difftime(now, os.time(os.date("!*t", now)))
 end
 
 -- Return a timezone string in ISO 8601:2000 standard form (+hhmm or -hhmm)
-local function get_tzoffset()  
-  local h, m = math.modf(get_timezone() / 3600)
+function Util:get_tzoffset()
+  local h, m = math.modf(Util:get_timezone() / 3600)
   return string.format("%+.4d", 100 * h + 60 * m)
 end
 
-local function html_entity_decode(str)  
+function Util:html_entity_decode(str)
   local a,b = str:gsub("%%20", " ")
   a,b = a:gsub("%%5B", "[")
   a,b = a:gsub("%%5D", "]")  
   return a
 end
 
-local function get_extension(file)
+function Util:get_extension(file)
     return file:match("%.(%a+)$")
 end
 
-
-local function trim(s)
+function Util:trim(s)
   return (s:gsub("^%s*(.-)%s*$", "%1"))
 end
 
-local function split(str, pat)
+function Util:split(str, pat)
    local t = {}  -- NOTE: use {n = 0} in Lua-5.0
    local fpat = "(.-)" .. pat
    local last_end = 1
@@ -180,14 +149,14 @@ local function split(str, pat)
 end
 
 -- Assumes "#comment" is a comment, "value  key_1 key_2"
-local function parse_config_file(filename)
-   local str = read_file(root .. filename)
-   local lines = split_lines(str)
+function Util:parse_config_file(filename)
+   local str = Util:read_file(root .. filename)
+   local lines = Util:split_lines(str)
    local t = {}
    local k, v = nil
    for _,l in ipairs(lines) do
       if not l:find("^(%s*)#") then
-        local a = split(l, "%s+")
+        local a = Util:split(l, "%s+")
            for i=2,#a do
               t[a[i]] = a[1]
            end
@@ -204,13 +173,27 @@ class "ActionServer"
 
   ActionServer.document_root = root .. "html"
   
+  local message = {
+    boolean_value = nil,
+    int_value = nil,
+
+    value_min_scaling = 0.0,
+    value_max_scaling = 1.0,
+
+    is_trigger = function() return true end,
+    is_switch = function() return false end,
+    is_rel_value = function() return false end,
+    is_abs_value = function() return false end
+  }
+
   function ActionServer:__init(address,port)
-   self.renoise = renoise 
+   self:index_action_names()
+
    -- create a server socket
    local server, socket_error =
      renoise.Socket.create_server(address, port)
-   
-   if socket_error then 
+
+   if socket_error then
      renoise.app():show_warning(
        "Failed to start the echo server: " .. socket_error)
    else
@@ -224,30 +207,49 @@ class "ActionServer"
   function ActionServer:socket_error(socket_error)
     renoise.app():show_warning(socket_error)
   end
-  
+
   function ActionServer:get_address()
     return self.server.local_address .. ':' .. self.server.local_port
   end
 
   function ActionServer:get_date()
-    return os.date("%a, %d %b %Y %X " .. get_tzoffset())
+    return os.date("%a, %d %b %Y %X " .. Util:get_tzoffset())
   end
 
   function ActionServer:socket_accepted(socket)
     log:info("Socket accepted")
   end
 
+  function ActionServer:find_action(action_name)
+    if table.find(ActionServer.action_names, action_name) then
+      log:info("Invoking: " .. action_name)
+      invoke_action(action_name, message)
+      return true
+    else
+      log:warn("Action not found: " .. action_name)
+    end
+    return false
+  end
+
+  function ActionServer:index_action_names()
+    ActionServer.action_names = available_actions()
+  end
+  
+  function ActionServer:get_action_names()
+   return ActionServer.action_names
+  end
+
   function ActionServer:parse_post_string(body)
-    if #trim(body) == 0 then return {} end
+    if #Util:trim(body) == 0 then return {} end
      local p = {}
      for k,v in body:gmatch("([^=&]+)=([^=&]+)") do
-       p[trim(k)] = trim(v)
+       p[Util:trim(k)] = Util:trim(v)
      end
      return p
   end
 
   function ActionServer:get_MIME(path)
-    local ext = get_extension(path)
+    local ext = Util:get_extension(path)
     local mime = ActionServer.mime_types[ext] or "text/plain"
     log:info("Extension: " .. ext .. "; Content-Type: " .. mime)
   end
@@ -268,7 +270,7 @@ class "ActionServer"
 
   function ActionServer:is_expandable(path)
     local extensions = table.create{"lua","html"}
-    local path_ext = get_extension(path)
+    local path_ext = Util:get_extension(path)
     return extensions:find(path_ext)
   end
 
@@ -292,12 +294,12 @@ class "ActionServer"
 
      parameters = parameters or {}
      local fullpath = self:get_htdoc(path)
-     local template = read_file(fullpath)
+     local template = Util:read_file(fullpath)
      local page = nil
 
      if self:is_expandable(path) then
        self:header("Cache-Control", "private, max-age=0")
-       page = expand(template, {L=self, renoise=renoise, P=parameters}, _G)
+       page = expand(template, {L=self, renoise=renoise, P=parameters, Util=Util}, _G)
      else
        self:header("Cache-Control", "private, max-age=3600")
        page = template
@@ -316,9 +318,9 @@ class "ActionServer"
 
   function ActionServer:socket_message(socket, message)
       print("\r\n----------MESSAGE RECEIVED----------")
-      local header, body = parse_message(message)
+      local header, body = Util:parse_message(message)
       local parameters = nil -- POST and GET variables
-      if #trim(body) > 0 then log:info("Body:" .. body) end
+      if #Util:trim(body) > 0 then log:info("Body:" .. body) end
       local path = nil
       local methods = table.create{"GET","POST","HEAD",
         "OPTIONS", "CONNECT", "PUT", "DELETE", "TRACE"}
@@ -327,7 +329,7 @@ class "ActionServer"
       if method ~= nil then
         method = method:upper()
         path = header:match("%s(.-)%s")
-        path = trim(html_entity_decode(path))
+        path = Util:trim(Util:html_entity_decode(path))
       else
         log:warn("No HTTP method received")
         return
@@ -344,7 +346,7 @@ class "ActionServer"
       -- handle index pages quickly
       local index_pages = table.create{"/index.html","/index.lua","/"}
       if index_pages:find(path) then
-          if path == "/" then path = "/index.html" end
+          if path == "/" then path = index_pages[1] end
           self:send_htdoc(socket, path)
           return
       end
@@ -359,7 +361,11 @@ class "ActionServer"
         else
           local action_name = string.sub(path:gsub('\/', ':'), 2)
           log:info ("Requested action:" .. action_name)
-          find_action(action_name)
+          local found = self:find_action(action_name)
+          if found then
+             self:send_htdoc(socket, index_pages[1], nil, parameters)
+             return
+          end
         end
      end
 
@@ -376,14 +382,124 @@ class "ActionServer"
 ]]--
 
   end
+  
+   function ActionServer:stop()
+      self.server:stop()
+   end
 
 -------------------------------------------
+local address = "0.0.0.0"
+local port = 80
+local INADDR_ANY = false
+
+function restore_default_configuration()
+    port = 80
+    address = "0.0.0.0"
+    INADDR_ANY = false
+end
 
 function start_server()
-    print("\r\n==========SERVER START===========")
-    ActionServer.mime_types = parse_config_file("/mime.types")
-    local address = "localhost"
-    local port = 80
-    echo_server = ActionServer(address, port)
+    print("\r\n==========STARTING SERVER===========")
+    ActionServer.mime_types = Util:parse_config_file("/mime.types")
+    action_server = ActionServer(address, port)
 --    renoise.app():open_url("http://"..address..":"..port)
+end
+
+function stop_server()
+   if action_server then
+      print("\r\n==========STOPPING SERVER===========")
+      action_server:stop()
+   end
+end
+
+-- todo hostnames
+local function is_valid_ip(str)
+   return str:match("^%d.%d.%d.%d$") or str == "localhost"
+end
+
+local function set_address(value)
+   if is_valid_ip(value) then
+      address = value
+   end
+end
+
+local function set_port(value)
+   port = value
+end
+
+function configure_server()
+   local vb = renoise.ViewBuilder()
+   local DEFAULT_DIALOG_MARGIN =
+    renoise.ViewBuilder.DEFAULT_DIALOG_MARGIN
+   local DEFAULT_CONTROL_SPACING =
+    renoise.ViewBuilder.DEFAULT_CONTROL_SPACING
+   local TEXT_ROW_WIDTH = 80
+   local temp = address
+
+   local content =
+     vb:column {
+       style = "invisible",
+       margin = DEFAULT_DIALOG_MARGIN,
+       spacing = DEFAULT_CONTROL_SPACING,
+
+       vb:row {
+          vb:text {
+            width = TEXT_ROW_WIDTH,
+            text = "INADDR_ANY"
+          },
+          vb:checkbox {
+            value = INADDR_ANY,
+            notifier = function(value)
+               INADDR_ANY = value
+               if value then
+                 vb.views.address_field.text = "0.0.0.0"
+               else
+                 vb.views.address_field.text = temp
+               end
+            end
+          },
+        },
+
+       vb:row {
+          vb:text {
+            width = TEXT_ROW_WIDTH,
+            text = "Address"
+          },
+          vb:textfield {
+            visible = not INADDR_ANY,
+            id = "address_field",
+            value = address,
+            notifier = function(value)
+              set_address(value)
+            end
+          },
+        },
+
+        vb:row {
+          vb:text {
+            width = TEXT_ROW_WIDTH,
+            text = "Port"
+          },
+          vb:valuebox {
+            value = port,
+            min = 0,
+            max = 65535,
+            notifier = function(value)
+              set_port(value)
+            end
+          }
+        }
+
+     }
+  local buttons = {"OK", "Default"}
+  local choice = renoise.app():show_custom_prompt("Configure ActionServer", content, buttons)
+  if choice == "Cancel" then
+--      restore_previous_configuration()
+  end
+  if choice == buttons[2] then
+      restore_default_configuration()
+  end
+  if choice then
+   start_server()
+  end
 end
