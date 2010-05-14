7b8e9729a26369d19929afce5b82289da9b31e0d
mueller.eduard <mueller.eduard@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-05-14 14:51:21 +0000

    - a bit more documentation for the audio script tests

--- branches/future/Audio/Effects/com.renoise.Example-Shape.xrnp/main.lua
+++ branches/future/Audio/Effects/com.renoise.Example-Shape.xrnp/main.lua
@@ -4,25 +4,41 @@
 
 local abs = math.abs
 
+local function shape_factors(amount)
+  return (2 * amount / (1 - amount)), (1.0 - (amount*0.5))
+end
+
+
+local function shape(input, factor)
+  return (1 + factor) * input / (1 + factor * abs(input)) 
+end
+  
+
+-------------------------------------------------------------------------------
+
 local parameters = {
   amount = 0.5
 }
 
-local function shape(input, k)
-  return (1 + k) * input / (1 + k * abs(input))
-end
-  
     
 -------------------------------------------------------------------------------
 
--- __init
+-- __init (parameter setup)
+
+-- register parameters for the device. can only be done here
 
 function __init(device)
   device:add_parameter("Amount", 0.0, 0.98, parameters.amount)
 end
 
 
--- __process_input
+-------------------------------------------------------------------------------
+
+-- __process_input (parameter changes)
+
+-- name: name of the parameter as it was registered
+-- value: new value name of the parameter
+
 
 function __process_input(name, value)
   if (name == "Amount") then
@@ -31,15 +47,19 @@ function __process_input(name, value)
 end
 
 
+-------------------------------------------------------------------------------
+
 -- __process_audio
 
+-- left, right: table of floats for inplace procesing
+-- num_frames: number of frames that are valid in left/right
+
 function __process_audio(left, right, num_frames)
-  local amount = parameters.amount
-  local k = 2 * amount / (1 - amount)
+  local factor, comp_gain = shape_factors(parameters.amount)
   
   for i = 1,num_frames do
-    left[i] = shape(left[i], k)
-    right[i] = shape(right[i], k)
+    left[i] = shape(left[i], factor) * comp_gain
+    right[i] = shape(right[i], factor) * comp_gain
   end
 end
 
