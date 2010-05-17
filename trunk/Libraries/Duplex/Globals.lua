110ec1aeb9e375b2112a7b605d936aa07ab15fdf
mueller.eduard <mueller.eduard@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-05-17 14:04:11 +0000

    some duplex refactoring while trying to find my way through it: - tabs to spaces (to use the internal script editor) - separate class and function defs - moved application implementations into a new folder

--- trunk/Libraries/Duplex/Globals.lua
+++ trunk/Libraries/Duplex/Globals.lua
@@ -2,8 +2,10 @@
 -- Duplex.Globals
 ----------------------------------------------------------------------------]]--
 
-MODULE_PATH = "./Duplex/"	
-NOTE_ARRAY = {"C-","C#","D-","D#","E-","F-","F#","G-","G#","A-","A#","B-"}
+-- internal Duplex consts
+
+MODULE_PATH = "./Duplex/"  
+NOTE_ARRAY = { "C-","C#","D-","D#","E-","F-","F#","G-","G#","A-","A#","B-" }
 
 DEVICE_OSC_PROTOCOL = 0
 DEVICE_MIDI_PROTOCOL = 1
@@ -12,18 +14,29 @@ MIDI_CC_MESSAGE = 6
 MIDI_NOTE_MESSAGE = 7
 OSC_MESSAGE = 8
 
-DEVICE_EVENT_BUTTON_PRESSED = 10	--	button event
-DEVICE_EVENT_BUTTON_RELEASED = 11	--	button event
-DEVICE_EVENT_VALUE_CHANGED = 12		--	slider, encoder event
+--  button event
+DEVICE_EVENT_BUTTON_PRESSED = 10  
+--  button event
+DEVICE_EVENT_BUTTON_RELEASED = 11  
+--  slider, encoder event
+DEVICE_EVENT_VALUE_CHANGED = 12    
 
-CONTROLLER_BUTTON = 20		--	bidirectional button (LED)
-CONTROLLER_ENCODER = 21		--	bidirectional encoder (LED)
-CONTROLLER_FADER = 22		--	manual fader (possibly with parameter-pickup, so we both recieve & transmit MIDI)
---CONTROLLER_MFADER = 23		--	motorized fader
---CONTROLLER_POT = 24			--	basic rotary encoder (MIDI input only)
+-- bidirectional button (LED)
+CONTROLLER_BUTTON = 20    
+--  bidirectional encoder (LED)
+CONTROLLER_ENCODER = 21  
+-- manual fader (possibly with parameter-pickup, so we both recieve & transmit MIDI)  
+CONTROLLER_FADER = 22    
+-- motorized fader
+CONTROLLER_MFADER = 23   
+-- basic rotary encoder (MIDI input only) 
+CONTROLLER_POT = 24      
 
 VERTICAL = 80
 HORIZONTAL = 81
+
+
+-- Renoise API consts
 
 RENOISE_DECIBEL = 1.4125375747681
 
@@ -33,43 +46,65 @@ DEFAULT_SPACING = renoise.ViewBuilder.DEFAULT_CONTROL_SPACING
 MUTE_STATE_OFF = 2
 MUTE_STATE_ACTIVE = 1
 
+
+--------------------------------------------------------------------------------
+-- helper functions
+--------------------------------------------------------------------------------
+
+-- compare:
 -- compare two numbers with variable precision
+
 function compare(val1,val2,precision)
-	val1 = math.floor(val1*precision)
-	val2 = math.floor(val2*precision)
-	return val1==val2 
+  val1 = math.floor(val1*precision)
+  val2 = math.floor(val2*precision)
+  return val1==val2 
 end
+
+
+-- get_master_track
 
 function get_master_track()
-	for i,v in pairs(renoise.song().tracks) do
-		if v.type == renoise.Track.TRACK_TYPE_MASTER then
-			return v
-		end
-	end
+  for i,v in pairs(renoise.song().tracks) do
+    if v.type == renoise.Track.TRACK_TYPE_MASTER then
+      return v
+    end
+  end
 end
+
+
+-- get_master_track_index
 
 function get_master_track_index()
-	for i,v in pairs(renoise.song().tracks) do
-		if v.type == renoise.Track.TRACK_TYPE_MASTER then
-			return i
-		end
-	end
+  for i,v in pairs(renoise.song().tracks) do
+    if v.type == renoise.Track.TRACK_TYPE_MASTER then
+      return i
+    end
+  end
 end
 
+
+-- deepcopy
+
 function deepcopy(object)
-    local lookup_table = {}
-    local function _copy(object)
-        if type(object) ~= "table" then
-            return object
-        elseif lookup_table[object] then
-            return lookup_table[object]
-        end
-        local new_table = {}
-        lookup_table[object] = new_table
-        for index, value in pairs(object) do
-            new_table[_copy(index)] = _copy(value)
-        end
-        return setmetatable(new_table, getmetatable(object))
+  local lookup_table = {}
+
+  local function _copy(object)
+    if type(object) ~= "table" then
+      return object
+    elseif lookup_table[object] then
+      return lookup_table[object]
     end
-    return _copy(object)
+    
+    local new_table = {}
+    lookup_table[object] = new_table
+    
+    for index, value in pairs(object) do
+      new_table[_copy(index)] = _copy(value)
+    end
+    
+    return setmetatable(new_table, getmetatable(object))
+  end
+  
+  return _copy(object)
 end
+
--- trunk/Libraries/Duplex/MessageStream.lua
+++ trunk/Libraries/Duplex/MessageStream.lua
@@ -17,51 +17,60 @@ This also offers us a "brute-force" method for terminating device communication
 
 --]]
 
-module("Duplex", package.seeall);
+
+--==============================================================================
 
 class 'MessageStream' 
 
 function MessageStream:__init()
 --print('"MessageStream"')
 
-	self.button_hold_time = 300	-- milliseconds
-	self.double_press_time = 100 -- milliseconds
+  self.button_hold_time = 300  -- milliseconds
+  self.double_press_time = 100 -- milliseconds
 
-	self.change_listeners = {}		-- sliders,encoders
-	self.press_listeners = {}		-- buttons
-	--self.release_listeners = {}		
-	--self.double_press_listeners = {}		
-	--self.hold_listeners = {}	
-	--self.combo_listeners = {}		
+  self.change_listeners = {}    -- sliders,encoders
+  self.press_listeners = {}    -- buttons
+  --self.release_listeners = {}    
+  --self.double_press_listeners = {}    
+  --self.hold_listeners = {}  
+  --self.combo_listeners = {}    
 
-	self.current_message = nil	-- most recent message (event handlers check this)
-	self.button_cache = nil		-- cache of recent events (used for double-press detection)
-	self.ignored_buttons = nil	-- temporarily exclude from interpretation
-	self.pressed_buttons = {}	-- currently pressed buttons, in order of arrival
+  self.current_message = nil  -- most recent message (event handlers check this)
+  self.button_cache = nil    -- cache of recent events (used for double-press detection)
+  self.ignored_buttons = nil  -- temporarily exclude from interpretation
+  self.pressed_buttons = {}  -- currently pressed buttons, in order of arrival
 
 end
+
+
+--------------------------------------------------------------------------------
 
 function MessageStream:idle_check()
-	-- flush cached messages when considered obsolete
-	-- check if pressed_buttons have been pressed for specified amount of time 
-	 -- print("MessageStream:idle_check()")
-
+  -- flush cached messages when considered obsolete
+  -- check if pressed_buttons have been pressed for specified amount of time 
+  -- print("MessageStream:idle_check()")
 end
+
+
+--------------------------------------------------------------------------------
 
 function MessageStream:add_listener(obj,evt_type,handler)
 --print('MessageStream: attaching event listener:'..evt_type)
-	if evt_type == DEVICE_EVENT_BUTTON_PRESSED then
-		table.insert(self.press_listeners,#self.press_listeners+1,{handler=handler,obj=obj})
-		--print("onpress handler added")
-	end
-	if evt_type == DEVICE_EVENT_VALUE_CHANGED then
-		table.insert(self.change_listeners,#self.change_listeners+1,{handler=handler,obj=obj})
-		--print("onpress handler added")
-	end
+  if evt_type == DEVICE_EVENT_BUTTON_PRESSED then
+    table.insert(self.press_listeners,#self.press_listeners+1,{handler=handler,obj=obj})
+    --print("onpress handler added")
+  end
+  if evt_type == DEVICE_EVENT_VALUE_CHANGED then
+    table.insert(self.change_listeners,#self.change_listeners+1,{handler=handler,obj=obj})
+    --print("onpress handler added")
+  end
 
 --print("Number of listeners after addition:", #self.press_listeners,#self.change_listeners)
 
 end
+
+
+--------------------------------------------------------------------------------
 
 -- remove event listener
 -- @return (boolean) true if successfull. false if not
@@ -69,67 +78,71 @@ end
 function MessageStream:remove_listener(obj,evt_type)
 --print("MessageStream:remove_listener:",obj,evt_type)
 
-	if evt_type == DEVICE_EVENT_BUTTON_PRESSED then
-		for i,listener in ipairs(self.press_listeners) do
-			if (obj == listener.obj) then
-				table.remove(self.press_listeners,i)
-				return true
-			end
-		end
-	end
-	if evt_type == DEVICE_EVENT_VALUE_CHANGED then
-		for i,listener in ipairs(self.change_listeners) do
-			if (obj == listener.obj) then
-				table.remove(self.change_listeners,i)
-				return true
-			end
-		end
-	end
-	return false
+  if evt_type == DEVICE_EVENT_BUTTON_PRESSED then
+    for i,listener in ipairs(self.press_listeners) do
+      if (obj == listener.obj) then
+        table.remove(self.press_listeners,i)
+        return true
+      end
+    end
+  end
+  if evt_type == DEVICE_EVENT_VALUE_CHANGED then
+    for i,listener in ipairs(self.change_listeners) do
+      if (obj == listener.obj) then
+        table.remove(self.change_listeners,i)
+        return true
+      end
+    end
+  end
+  return false
 
 end
 
 
+--------------------------------------------------------------------------------
+
 function MessageStream:input_message(msg)
 --print('MessageStream: event was recieved:',msg)
-	self.current_message = msg
-	if (msg.input_method == CONTROLLER_ENCODER) or (msg.input_method == CONTROLLER_FADER) then
-		--print('MessageStream: event was recieved:',msg.value)
-		--if msg.value == msg.max then
-		for _,listener in ipairs(self.change_listeners)  do 
-			listener.handler() 
-		end
+  self.current_message = msg
+  if (msg.input_method == CONTROLLER_ENCODER) or 
+     (msg.input_method == CONTROLLER_FADER) then
+    --print('MessageStream: event was recieved:',msg.value)
+    --if msg.value == msg.max then
+    for _,listener in ipairs(self.change_listeners)  do 
+      listener.handler() 
+    end
 
-		--for _,handler in ipairs(self.change_listeners)  do handler() end
-		--end		
-	elseif msg.input_method == CONTROLLER_BUTTON then
-		-- if it's listed in ignored_buttons
-			-- remove from ignored_buttons and exit
-		-- else if it's value match the min/max value..
-		if msg.value == msg.max then
-			-- interpret this as pressed
-				-- check if this button has been pressed recently
-					-- invoke double_press, and add to ignored_buttons (so the release won't trigger as well)
-				-- else, add to pressed_buttons
-				-- todo: check if already pressed, and skip adding
+    --for _,handler in ipairs(self.change_listeners)  do handler() end
+    --end    
+  elseif msg.input_method == CONTROLLER_BUTTON then
+    -- if it's listed in ignored_buttons
+      -- remove from ignored_buttons and exit
+    -- else if it's value match the min/max value..
+    if msg.value == msg.max then
+      -- interpret this as pressed
+        -- check if this button has been pressed recently
+          -- invoke double_press, and add to ignored_buttons (so the release won't trigger as well)
+        -- else, add to pressed_buttons
+        -- todo: check if already pressed, and skip adding
 
-				-- table.insert(self.pressed_buttons,#self.pressed_buttons+1,msg)
-				-- print_r(self.pressed_buttons)
+        -- table.insert(self.pressed_buttons,#self.pressed_buttons+1,msg)
+        -- print_r(self.pressed_buttons)
 
-				-- broadcast to listeners
-				for _,listener in ipairs(self.press_listeners)  do 
-					listener.handler() 
-				end
+        -- broadcast to listeners
+        for _,listener in ipairs(self.press_listeners)  do 
+          listener.handler() 
+        end
 
-			-- check other held buttons:
-				-- if combination is matched, invoke combination_press, and add held buttons to ignored_buttons (so the release won't trigger)
-		else 
-			if msg.value == msg.minimum then
-				-- interpret this as release
-					-- remove from pressed_buttons
-			end
-		end
-	end
+      -- check other held buttons:
+        -- if combination is matched, invoke combination_press, and add held buttons 
+        -- to ignored_buttons (so the release won't trigger)
+    else 
+      if msg.value == msg.minimum then
+        -- interpret this as release
+          -- remove from pressed_buttons
+      end
+    end
+  end
 end
 
 
