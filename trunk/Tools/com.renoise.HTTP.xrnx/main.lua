a76fe6efea38750d1f2ab8b15fe68e2d7c4af7a4
marvintjon <marvintjon@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-05-23 10:21:55 +0000

    Multiple download test

--- trunk/Tools/com.renoise.HTTP.xrnx/main.lua
+++ trunk/Tools/com.renoise.HTTP.xrnx/main.lua
@@ -42,27 +42,34 @@ local requests = table.create()
 local active = true
 
 class "Request"
+
+Request.GET = "GET"
+Request.POST = "POST"
+Request.HEAD = "HEAD"
+Request.OPTIONS = "OPTIONS"
+
 function Request:__init(url, method)
-  
+  method = method or Request.GET
   local parsed_url = Util:parse(url)
-  
+
   local get_request = string.format(
-    "GET %s HTTP/1.1\nHost: %s\r\n\r\n", 
-    parsed_url.path, parsed_url.host)
-  
+    "%s %s HTTP/1.1\nHost: %s\r\n\r\n",
+    method, parsed_url.path, parsed_url.host)
+
   local client = renoise.Socket.create_client(
-    parsed_url.host, 80,  renoise.Socket.PROTOCOL_TCP)        
-  
+    parsed_url.host, 80,  renoise.Socket.PROTOCOL_TCP)
+
   local ok, err = client:send(get_request)
 
+  self.complete = false
   self.url = url
   self.contents = table.create()
   self.callback = function() end
   self.reader = SocketReader(client)
   self.length = 0
-  self.header = table.create {}  
-  
-  self:get_header()  
+  self.header = table.create {}
+
+  self:get_header()
 end
 
 function Request:get_header()      
@@ -78,14 +85,15 @@ function Request:get_header()
       
       self.header:insert(line)
     end
-  
+
     log:info("=== HEADER ===")
     rprint(self.header)
+    print("\r\n")
 end
 
 function Request:do_callback()
-  log:info("=== CONTENT ===")
-  rprint(self.contents)
+--  log:info("=== CONTENT ===")
+--  rprint(self.contents)
   self.callback()
   log:info(self.url .. " has completed.")  
 end
@@ -102,7 +110,7 @@ function Request:get_content(mode, timeout)
   else
     self.contents:insert(buffer)
     self:inc_length(#buffer)
-    log:info(string.format("%d bytes read", self.length))
+    log:info(string.format("%d bytes read (%s)", self.length, self.url))
     return #buffer
   end
 end
@@ -114,9 +122,11 @@ end
 -- Read a few bytes from every request
 local function read()
     for k,request in ipairs(requests) do
-      local bytes_received = request:get_content(1460, 10)
-      if (not bytes_received) then
-        requests[k] = nil
+      if not request.complete then
+        local bytes_received = request:get_content(1460, 10)
+        if (not bytes_received) then
+          request.complete = true
+        end
       end
     end
     return true
@@ -131,9 +141,14 @@ end
 
 -------------------------------------------------------------------------------
 
+function http(url, method)
+   requests:insert(Request(url, method))
+end
+
 function start()
-  requests:insert(Request("http://www.renoise.com/download/checkversion.php"))
---  request("http://www.renoise.com/download/")
+  http("http://www.renoise.com/download/checkversion.php")
+  http("http://www.renoise.com/download/")
+  http("http://www.renoise.com/")
   --request("http://nl.archive.ubuntu.com/ubuntu-cdimages/10.04/release/ubuntu-10.04-dvd-amd64.iso")
 end
 
