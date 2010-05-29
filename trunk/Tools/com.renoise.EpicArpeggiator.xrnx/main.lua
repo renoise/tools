a883fce0f5a011d2e9c2c5cec4bbb8e5ccbb5f5f
mueller.eduard <mueller.eduard@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-05-29 10:36:16 +0000

    - menu cosmetics: entries which open dialogs should end with "..."

--- trunk/Tools/com.renoise.EpicArpeggiator.xrnx/main.lua
+++ trunk/Tools/com.renoise.EpicArpeggiator.xrnx/main.lua
@@ -2,14 +2,12 @@
 -- tool setup
 -- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-renoise.tool():add_menu_entry {
-   name = "Main Menu:Tools:Epic Arpeggiator",
-   invoke = function() open_arp_dialog() end
-}
-
-
-
 require "globals"
 require "tone_matrix"
 require "pattern_processing"
 require "gui"
+
+renoise.tool():add_menu_entry {
+   name = "Main Menu:Tools:Epic Arpeggiator...",
+   invoke = open_arp_dialog
+}
