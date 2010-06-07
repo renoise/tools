deb69df16d95ab42d32958b7d1d2d700710541b9
vincent.voois@gmail.com <vincent.voois@gmail.com@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-06-07 20:05:58 +0000

    -Cleaned up the dirty code. -Added simple status dialog -JOIN and Rough messaging now possible through raw IRC commands (RFC)

--- trunk/Tools/com.renoise.IRCclient.xrnx/main.lua
+++ trunk/Tools/com.renoise.IRCclient.xrnx/main.lua
@@ -1,54 +1,14 @@
 
 renoise.tool():add_menu_entry {
   name = "Main Menu:Tools:Connect to #Renoise@espernet",
-  invoke = function() main() end
+  invoke = function() client_login() end
 }
 
 local irc_host = "canis.esper.net"
 local irc_port = 6667
 local socket_timeout = 1000
 local server, server_error
-
-function main()
-  ------------ ircclient.lua
-  local start_time = os.clock()
-
-  repeat
-    server, server_error = renoise.Socket.create_client(
-      irc_host, irc_port)
-    -- wait a bit until the controller started...
-  until (server or os.clock() - start_time > 2.0) 
-  if server then
-    print_server_replies()
-    text_control()  
-
---    print_server_replies()  
---    server:send("PASS guest\r\n")
-    server:send("NICK vvoois\r\n")
-    print ("nick sent")
-    print_server_replies()  
-    server:send("USER voois 8 * : Vincent Voois\r\n")
-    print ("user sent")
-    local reply = print_server_replies()  
-    local dopos = nil
-    if reply ~= nil then
-      dopos = string.find(reply, "PING")
-    end
-    if dopos ~= nil then
-      reply = string.gsub(reply, "PING", "PONG").."\r\n"
-      server:send(reply)
-      print ("send:"..reply)
-      print ("pong sent")
-    end
-    print_server_replies()  
-    server:send("JOIN #renoise")
-    print ("join sent")
---    while not server:receive("*l"):find("004") do end
---    server:send("PRIVMSG #Renoise :Testing my Lua socket client\n")
---    print_server_replies()  
-   end
---  text_control()  
-end
+local rirc = nil
 
 
 function print_server_replies()
@@ -61,17 +21,19 @@ function print_server_replies()
       last_line = command_line
     end
     local command_line, status = server:receive("*l", socket_timeout)
+    -- If a ping is received, reply immediately
     if command_line ~= nil then
       dpos = string.find(command_line, "PING")
     end
     if  dpos ~= nil then
       command_line = string.gsub(command_line, "PING", "PONG").."\r\n"
       server:send(command_line)
-      print ("send:"..command_line)
-      print ("pong sent")
+--      print ("send:"..command_line)
+--      print ("pong sent")
     end
-
-    print (command_line)
+    if command_line ~= nil then
+      rirc.views.console_frame.text = rirc.views.console_frame.text..command_line.."\r\n"
+    end
   until command_line == nil
 
   return last_line
@@ -81,18 +43,44 @@ function send_and_receive(msg)
     server:send(msg)
     print_server_replies()  
 end
+
+function set_nick(nick)
+  local COMMAND = "NICK "..nick.."\r\n"
+  print (COMMAND)
+  send_and_receive(COMMAND)
+end
+function register_user(user, real_name)
+  local COMMAND = "USER "..user.." 8 * : "..real_name.."\r\n"
+  print (COMMAND)
+  send_and_receive(COMMAND)
+end
+
+function join_channel(channel)
+  local COMMAND = "JOIN "..channel.."\r\n"
+  print (COMMAND)
+  send_and_receive(COMMAND)
+end
+
+function send_command (command)
+  if command ~= nil then
+    local COMMAND = command.."\r\n"
+    send_and_receive(COMMAND)
+  end
+end
+
 --------------------------------------------------------------------------------
 
--- available_controls
+-- GUI dialog
 
-function text_control()
-
+function client_login()
+  rirc = irc_dialog()
+  
   local function show_status(message)
     renoise.app():show_status(message); print(message)
   end
   
   -- we memorize a reference to the dialog this time, to close it
-  local control_example_dialog = nil
+  local login_dialog = nil
   
   local vb = renoise.ViewBuilder()
   
@@ -108,17 +96,71 @@ function text_control()
   -- CONTROL ROWS
   
   -- textfield
-  local textfield_row = vb:row {
-    vb:text {
-      width = TEXT_ROW_WIDTH,
-      text = "vb:textfield"
+  local textfield_row = vb:column{
+    vb:row {
+      vb:text {
+        width = TEXT_ROW_WIDTH,
+        text = "username"
+      },
+      vb:textfield {
+        width = 300,
+        text = "guest",
+        id = 'irc_user_name',
+        notifier = function(text)
+        end
+      },
     },
-    vb:textfield {
-      width = 300,
-      text = "PONG ",
-      notifier = function(text)
-          send_and_receive(text)
-      end
+    vb:row{
+      vb:text {
+        width = TEXT_ROW_WIDTH,
+        text = "real name"
+      },
+      vb:textfield {
+        width = 300,
+        text = "Vincent Voois",
+        id = 'irc_real_name',
+        notifier = function(text)
+        end
+      },
+    },
+    vb:row{
+      vb:text {
+        width = TEXT_ROW_WIDTH,
+        text = "nickname"
+      },
+      vb:textfield {
+        width = 300,
+        text = "vvoois",
+        id = 'irc_nick_name',
+        notifier = function(text)
+        end
+      },
+    },
+    vb:row{
+      vb:button {
+        width = TEXT_ROW_WIDTH,
+        text = "Login",
+        notifier = function(text)
+          local start_time = os.clock()
+        
+          repeat
+            server, server_error = renoise.Socket.create_client(
+              irc_host, irc_port)
+            -- wait a bit until the controller started...
+          until (server or os.clock() - start_time > 2.0) 
+          if server then
+            print_server_replies()
+            set_nick(vb.views.irc_nick_name.text)            
+            print_server_replies()
+            register_user(vb.views.irc_user_name.text, vb.views.irc_real_name.text)
+            print_server_replies()
+            login_dialog:close()
+        --    while not server:receive("*l"):find("004") do end
+        --    server:send("PRIVMSG #Renoise :Testing my Lua socket client\n")
+        --    print_server_replies()  
+           end
+        end
+      },
     }
   }
   -- MAIN CONTENT & LAYOUT
@@ -138,9 +180,90 @@ function text_control()
   
   -- DIALOG
   
-  control_example_dialog = renoise.app():show_custom_dialog(
-    "Controls", dialog_content
+  login_dialog = renoise.app():show_custom_dialog(
+    "IRC Client", dialog_content
   )
 
 end
 
+
+
+function irc_dialog()
+
+  local function show_status(message)
+    renoise.app():show_status(message); print(message)
+  end
+  
+  -- we memorize a reference to the dialog this time, to close it
+  local irc_dialog = nil
+  
+  local vb = renoise.ViewBuilder()
+  
+  local DIALOG_MARGIN = renoise.ViewBuilder.DEFAULT_DIALOG_MARGIN
+  local CONTENT_SPACING = renoise.ViewBuilder.DEFAULT_CONTROL_SPACING
+  local CONTENT_MARGIN = renoise.ViewBuilder.DEFAULT_CONTROL_MARGIN
+  local DEFAULT_CONTROL_HEIGHT = renoise.ViewBuilder.DEFAULT_CONTROL_HEIGHT
+  local DEFAULT_MINI_CONTROL_HEIGHT = renoise.ViewBuilder.DEFAULT_MINI_CONTROL_HEIGHT
+  local CONTROL_MARGIN = renoise.ViewBuilder.DEFAULT_CONTROL_MARGIN
+    
+  local TEXT_ROW_WIDTH = 80
+
+
+  -- CONTROL ROWS
+  
+  -- textfield
+  local textfield_row = vb:column{
+    vb:row {
+      margin = CONTROL_MARGIN,
+      vb:multiline_text{
+        width = 500,
+        height = 300, 
+        id = 'console_frame',
+        text = ""
+      }
+    },
+    vb:row{
+      vb:text {
+        width = TEXT_ROW_WIDTH,
+        text = "text / command"
+      },
+      vb:textfield {
+        width = 300,
+        text = "",
+        notifier = function(text)
+          send_command(text)
+        end
+      },
+    },
+    vb:row{
+      vb:button {
+        width = TEXT_ROW_WIDTH,
+        text = "Send",
+        notifier = function(text)
+          send_command(text)
+        end
+      },
+    }
+  }
+  -- MAIN CONTENT & LAYOUT
+  
+  local dialog_content = vb:column {
+    margin = DIALOG_MARGIN,
+    spacing = CONTENT_SPACING,
+    uniform = true,
+
+    vb:column {
+      textfield_row, 
+      vb:space { height = 2*CONTENT_SPACING },
+    },
+    
+  }
+  
+  
+  -- DIALOG
+  
+  irc_dialog = renoise.app():show_custom_dialog(
+    "IRC Client", dialog_content
+  )
+  return vb
+end
