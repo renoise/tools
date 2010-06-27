f63e2839bee7830dcb21ef0c69acd397e81b8e71
mueller.eduard <mueller.eduard@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-06-27 03:53:15 +0000

    - GUI cosmetics

--- branches/future/Tools/com.renoise.HTTPBugReporter.xrnx/main.lua
+++ branches/future/Tools/com.renoise.HTTPBugReporter.xrnx/main.lua
@@ -26,6 +26,7 @@ local bug_report = renoise.Document.create {
   email = "your email",
   name = "your name" ,
   severe = false,
+  add_log = false,
   log = "",
 }
 
@@ -72,43 +73,48 @@ end
 --  Gui
 -------------------------------------------------------------------------------
 
-local vb = nil
 local dialog = nil
 
 
 -- show_confirmation
 
 function show_confirmation()
-  vb.views.confirmation.visible = true
-  vb.views.confirmation.text = 
-    "Your bug report has been received. Thanks!"
+  renoise.app():show_prompt(
+    "Report Submited",
+    "Your bug report has been received. Thanks!", 
+    {"OK"}
+  )
+  
+  dialog:close()
 end
 
 
 -- show_error
 
 function show_error()
-  vb.views.confirmation.visible = true
-  vb.views.confirmation.text = 
-    "Error while sending your bug report. How ironic!"
+  renoise.app():show_error(
+    "Error while sending your bug report. How ironic!")
+
+  dialog:close()
 end
 
 
 -- start
 
 function start()
-  if dialog and dialog.visible then
+  if (dialog and dialog.visible) then
     dialog:show()
     return
   end
 
-  vb = renoise.ViewBuilder()
+  local vb = renoise.ViewBuilder()
+  
+  local DEFAULT_MARGIN = renoise.ViewBuilder.DEFAULT_CONTROL_MARGIN  
+  local DEFAULT_SPACING = renoise.ViewBuilder.DEFAULT_CONTROL_SPACING
+  local DEFAULT_DILAOG_SPACING = renoise.ViewBuilder.DEFAULT_DIALOG_SPACING
   
   local DIALOG_BUTTON_HEIGHT = renoise.ViewBuilder.DEFAULT_DIALOG_BUTTON_HEIGHT
 
-  local DEFAULT_MARGIN = renoise.ViewBuilder.DEFAULT_CONTROL_MARGIN  
-  local DEFAULT_SPACING = 5   
-  
   local topic_group = vb:column {
     style = "group",
     margin = DEFAULT_MARGIN,      
@@ -121,7 +127,6 @@ function start()
     },
 
     vb:row {
-      spacing = DEFAULT_SPACING,
       width = "100%",
       
       vb:popup {
@@ -158,6 +163,8 @@ function start()
         end        
       },
     
+      vb:space { width = DEFAULT_SPACING},
+      
       vb:text {
         id = "other_topic_text",
         text = "namely:",
@@ -172,7 +179,8 @@ function start()
     }
   }  
   
-  local form_group = vb:column {      
+  local form_group = vb:column {  
+    id = "form_group",
     style = "group",
     margin = DEFAULT_MARGIN,      
     spacing = DEFAULT_SPACING,
@@ -182,25 +190,24 @@ function start()
        font = "bold",
        text = "Summary:"
     },
-
     vb:textfield {
       bind = bug_report.summary
-    },           
-
+    },        
+       
+    vb:space { height = DEFAULT_SPACING},
+    
     vb:text {
        font = "bold",
-       text = "Description (textbox):"
+       text = "Description:"
     },
-
     vb:multiline_textfield {
        bind = bug_report.description,
-       height = 80,
+       height = 80
     },      
     
     vb:row {       
       vb:checkbox {
-        bind = bug_report.severe,
-        value = false
+        bind = bug_report.severe
       },  
       vb:text {
         text = "Renoise crashed or the system became unresponsive."
@@ -209,18 +216,17 @@ function start()
   }  
     
   local optional_group = vb:column {
+    id = "optional_group",
     style = "group",
     margin = DEFAULT_MARGIN,      
     spacing = DEFAULT_SPACING,
-
 
     vb:horizontal_aligner {
       mode = "justify",
       
       vb:row {
         vb:checkbox {
-          id = "add_log_checkbox",
-          value = true
+          bind = bug_report.add_log
         },
         vb:text {
           text = "Include log with bug report"
@@ -230,6 +236,7 @@ function start()
       vb:button {
         text = "Read log",
         notifier = function() 
+          -- TODO
         end
       },        
     },
@@ -239,8 +246,9 @@ function start()
       vb:checkbox {
         id = "subscribe_checkbox",
         notifier = function(value)
-           vb.views.subscribe_column.visible = value
-           vb.views.subscribe_column:resize()
+          vb.views.subscribe_column.visible = value
+          vb.views.optional_group:resize()
+          vb.views.dialog_content:resize()
         end
       },
       
@@ -250,7 +258,7 @@ function start()
     },
     
     vb:column {
-      id="subscribe_column",
+      id = "subscribe_column",
       visible = false,
 
       vb:textfield {
@@ -266,54 +274,41 @@ function start()
   }  
   
   local form = vb:column {
-      margin = DEFAULT_MARGIN,      
-      spacing = DEFAULT_SPACING,
-      uniform = true, 
-      
-    vb:row {
-      margin = DEFAULT_MARGIN,            
-
-      vb:text {
-        text = "To submit a bug report, please fill in this form."
-      },
+    margin = DEFAULT_MARGIN,      
+    spacing = DEFAULT_DILAOG_SPACING,
+    uniform = true, 
+    
+    vb:text {
+      text = "To submit a bug report, please fill in this form." 
     },
     
     topic_group,
-    
     form_group, 
-    
     optional_group
   }
   
-  local dialog_content = vb:horizontal_aligner {
-    mode = "center",
+  local dialog_content = vb:column {
+    id = "dialog_content",
+    margin = DEFAULT_MARGIN,
     width = 200,
-    vb:column {
-      vb:row { 
-        style = "body",
-        margin = 5,
-        spacing = 5,
-        form 
-      },
-      vb:row {
-        margin = 5,
-        spacing = 5,
-        vb:button {
-          text = "Submit",
-          height = DIALOG_BUTTON_HEIGHT,
-          notifier = function() 
-            submit()
-          end
-        }, 
-        vb:text {
-          id = "confirmation",
-          visible = false          
-        }
-      }
+      
+    form,
+
+    vb:horizontal_aligner {
+      mode = "center",
+      margin = DEFAULT_MARGIN,
+  
+      vb:button {
+        text = "Submit",
+        height = DIALOG_BUTTON_HEIGHT,
+        notifier = function() 
+          submit()
+        end
+      }, 
     }
   }
 
-  dialog = renoise.app():show_custom_dialog("Bug Reporter",
-    dialog_content);
+  dialog = renoise.app():show_custom_dialog(
+    "Bug Reporter", dialog_content)
   
 end
