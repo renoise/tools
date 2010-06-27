4b011c07ca8c01f1f9e9cc60b1820ab8e7c80999
kunitoki <kunitoki@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-06-27 10:09:17 +0000

    - added a single Duplex include file to ease new inclusion list without touching the tool

---
+++ Libraries/Duplex/Duplex.lua
@@ -0,0 +1,31 @@
+--[[--------------------------------------------------------------------------
+
+ The Duplex Library
+
+--------------------------------------------------------------------------]]--
+
+-- includes
+
+require "Duplex/Application"
+require "Duplex/Globals"
+require "Duplex/MessageStream"
+require "Duplex/Display"
+require "Duplex/Canvas"
+require "Duplex/UIComponent"
+require "Duplex/UIToggleButton"
+require "Duplex/UISlider"
+require "Duplex/UISpinner"
+require "Duplex/ControlMap"
+require "Duplex/Device"
+require "Duplex/MIDIDevice"
+require "Duplex/OscDevice"
+--require "Duplex/Scheduler"
+
+require "Duplex/Applications/Browser"
+require "Duplex/Applications/MixConsole"
+require "Duplex/Applications/PatternMatrix"
+require "Duplex/Applications/MatrixTest"
+
+require "Duplex/Controllers/Launchpad/Launchpad"
+require "Duplex/Controllers/Ohm64/Ohm64"
+
