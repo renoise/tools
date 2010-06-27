aeeb98a60f0ef2b266298620e4377428eddf206f
kunitoki <kunitoki@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-06-27 10:09:17 +0000

    - added a single Duplex include file to ease new inclusion list without touching the tool

--- trunk/Tools/com.renoise.DuplexBrowser.xrnx/main.lua
+++ trunk/Tools/com.renoise.DuplexBrowser.xrnx/main.lua
@@ -6,27 +6,7 @@ Duplex_browser.lua
 
 -- includes
 
-require "Duplex/Application"
-require "Duplex/Globals"
-require "Duplex/MessageStream"
-require "Duplex/Display"
-require "Duplex/Canvas"
-require "Duplex/UIComponent"
-require "Duplex/UIToggleButton"
-require "Duplex/UISlider"
-require "Duplex/UISpinner"
-require "Duplex/ControlMap"
-require "Duplex/Device"
-require "Duplex/MIDIDevice"
-require "Duplex/OscDevice"
---require "Duplex/Scheduler"
-
-require "Duplex/Applications/Browser"
-require "Duplex/Applications/MixConsole"
-require "Duplex/Applications/PatternMatrix"
-require "Duplex/Applications/MatrixTest"
-
-require "Duplex/Controllers/Launchpad/Launchpad"
+require "Duplex/Duplex"
 
 
 -------------------------------------------------------------------------------
