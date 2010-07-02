6f71cab5d3af36e5e2020e059c83db834e0c2297
vincent.voois@gmail.com <vincent.voois@gmail.com@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-07-02 22:13:23 +0000

    -Chopped in the PC keyboard routine... made the routine a smaller smaller.

--- Tools/__in progress__/com.renoise.NoteRouter.xrnx/globals.lua
+++ Tools/__in progress__/com.renoise.NoteRouter.xrnx/globals.lua
@@ -7,6 +7,9 @@ PATTERN_EDITOR = 2
 record_destination = MAPPING
 tracks = {}
 note_to_track = {}
+low_key = {'z','s','x','d','c','v','g','b','h','n','j','m'}
+mid_key = {'comma','l','period',';','/','fill','fill','fill','fill','fill','fill','fill'}
+high_key = {'q','2','w','3','e','r','5','t','6','y','7','u'}
 
 nr_debug = false
 
