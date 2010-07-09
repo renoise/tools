7d12fd1f35b71c242b24eadc8ccfedcf211c839d
mueller.eduard <mueller.eduard@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-07-09 15:44:10 +0000

    duplex: - disable loopback_received_messages also for the Behringer BFR - updated comments in the BFC

--- trunk/Tools/com.renoise.Duplex.xrnx/Duplex/Controllers/BCR-2000/BCR-2000.lua
+++ trunk/Tools/com.renoise.Duplex.xrnx/Duplex/Controllers/BCR-2000/BCR-2000.lua
@@ -3,6 +3,22 @@
 ----------------------------------------------------------------------------]]--
 
 -- default configuration of the BCR-2000
+-- uses a custom device class, a control map and Mixer and Effect applications
+
+
+--==============================================================================
+
+class "BFR2000" (MidiDevice)
+
+function BFR2000:__init(name, message_stream)
+  TRACE("BFR2000:__init", name, message_stream)
+
+  MidiDevice.__init(self, name, message_stream)
+
+  -- the BFR can not handle looped back messages correctly, so we disable 
+  -- sending back messages we got from the BFR, in order to break feedback loops...
+  self.loopback_received_messages = false
+end
 
 
 --------------------------------------------------------------------------------
@@ -15,7 +31,7 @@ duplex_configurations:insert {
 
   -- device properties
   device = {
-    class_name = nil,          
+    class_name = "BFR2000",          
     display_name = "BCR-2000",
     device_name = "BCR2000",
     control_map = "Controllers/BCR-2000/BCR-2000.xml",
