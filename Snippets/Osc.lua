070a552cdc491e50ff395244102162d239ef6966
mueller.eduard <mueller.eduard@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-07-12 13:43:07 +0000

    - OSC snippets and documentation finetuning

--- Snippets/Osc.lua
+++ Snippets/Osc.lua
@@ -4,6 +4,10 @@
 
 error("do not run this file. read and copy/paste from it only...")
 
+-- create some handy shortcuts
+local OscMessage = renoise.Osc.Message
+local OscBundle = renoise.Osc.Bundle
+
 
 -------------------------------------------------------------------------------
 ---- Osc server (receive Osc from one or more clients)
@@ -14,34 +18,41 @@ local server, socket_error = renoise.Socket.create_server(
    
 if (socket_error) then 
   app:show_warning(("Failed to start the " .. 
-    "OSC server: '%s'"):format(socket_error))
+    "OSC server. Error: '%s'"):format(socket_error))
   return
 end
 
 server:run {
   socket_message = function(socket, data)
-    local message_or_bundle, osc_error = 
-      renoise.Osc.from_binary_data(data)
+    -- decode the data to Osc
+    local message_or_bundle, osc_error = renoise.Osc.from_binary_data(data)
     
+    -- show what we've got
     if (message_or_bundle) then
       if (type(message_or_bundle) == "Message") then
-        print(("Got OSC message: '%s'"):format(
-          tostring(message_or_bundle)))
+        print(("Got OSC message: '%s'"):format(tostring(message_or_bundle)))
 
       elseif (type(message_or_bundle) == "Bundle") then
-        print(("Got OSC bundle: '%s'"):format(
-          tostring(message_or_bundle)))
+        print(("Got OSC bundle: '%s'"):format(tostring(message_or_bundle)))
       
       else
         -- never will get in here
       end
       
     else
-      print(("Got an invalid or some data which is not an " ..
-        "OSC message at all. error: '%s'"):format(osc_error))
+      print(("Got invalid OSC data, or data which is not " .. 
+        "OSC data at all. Error: '%s'"):format(osc_error))
     end
+    
+    socket:send(("%s:%d: Thank you so much for the OSC message. " ..
+      "Here's one in return:"):format(socket.peer_address, socket.peer_port))
+      
+    socket:send(OscMessage("/flowers"))
   end    
 }
+
+-- shut off the server at any time with:
+-- server:close()
 
 
 -------------------------------------------------------------------------------
@@ -51,36 +62,40 @@ server:run {
 local client, socket_error = renoise.Socket.create_client(
   "localhost", 8008, renoise.Socket.PROTOCOL_UDP)
    
-if socket_error then 
+if (socket_error) then 
   app:show_warning(("Failed to start the " .. 
-    "OSC client: '%s'"):format(socket_error))
+    "OSC client. Error: '%s'"):format(socket_error))
   return
 end
 
--- create some handy shortcuts
-local OscMessage = renoise.Osc.Message
-local OscBundle = renoise.Osc.Bundle
-
 -- construct and send messages
-local message = OscMessage("/someone/transport/start")
+client:send(
+  OscMessage("/someone/transport/start")
+)
 
-client:send(message)
-
-message = OscMessage("/someone/transport/bpm", {
-  {tag="f", value=127.5}
-})
-
-client:send(message)
+client:send(
+  OscMessage("/someone/transport/bpm", { 
+    {tag="f", value=127.5} 
+  })
+)
 
 -- construct and send bundles
-local bundle = OscBundle(os.clock(), 
-  OscMessage("/someone/transport/start"))
-  
-client:send(bundle)
+client:send(
+  OscBundle(os.clock(), OscMessage("/someone/transport/start"))
+)
 
-bundle = OscBundle(os.clock(), {message, message})
-client:send(bundle)
+local message1 = OscMessage("/some/message")
+
+local message2 = OscMessage("/another/one", { 
+  {tag="b", value="with some blob data"},
+  {tag="s", value="and a string"} 
+})
+
+client:send(
+  OscBundle(os.clock(), {message1, message2})
+)
 
 
 --[[---------------------------------------------------------------------------
 ---------------------------------------------------------------------------]]--
+
