220b628dd7eca023f77fe25a9dc980e5d62c9866
fabio.napodano@gmail.com <fabio.napodano@gmail.com@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-07-13 14:53:16 +0000

    patternsplit: improved code style

--- Tools/com.renoise.PatternSplit.xrnx/main.lua
+++ Tools/com.renoise.PatternSplit.xrnx/main.lua
@@ -53,78 +53,86 @@ function split()
   -- pattern and the first of the new pattern, if it doesn't exists yet
   for num_track = 1, num_tracks do
   
-	for num_auto = 1, table.getn(new_pattern.tracks[num_track].automation) do
+    for num_auto = 1, table.getn(new_pattern.tracks[num_track].automation) do
 
-		local points = new_pattern.tracks[num_track].automation[num_auto].points
+  	  local points = new_pattern.tracks[num_track].automation[num_auto].points
 		
-		-- let's see if the line on which we are cutting already has a point in the curve
-		-- in such case, we are lucky and don't need to add any intermediate point..
-		if not new_pattern.tracks[num_track].automation[num_auto]:has_point_at(current_line) then
+      -- let's see if the line on which we are cutting already has a point
+      -- in the curve; in such case, we are lucky and don't need to add any
+      -- intermediate point..
+	  if not 
+		new_pattern.tracks[num_track].automation[num_auto]:has_point_at(current_line) 
+	  then
 		
-			-- let's see if the current automation curve has any point before the current_line and
-			-- any point after current_line.
-			local previous_point = 1
-			local next_point = table.getn(points)
+		-- let's see if the current automation curve has any point before the
+		-- current_line and any point after current_line.
+		  local previous_point = 1
+		  local next_point = table.getn(points)
 
-			for point = 1, table.getn(points) do
+		  for point = 1, table.getn(points) do
 				
-				if points[point].time < current_line and previous_point < points[point].time then
-				-- get the highest line which is less than current_line
-					previous_point = point
-				end 
-				if points[point].time > current_line and next_point > points[point].time then
-				-- get the lowest line which is greater than current_line
-					next_point = point				
-				end
-		
-			end
+		    if 
+		      points[point].time < current_line and 
+              previous_point < points[point].time 
+			then
+			  -- get the highest line which is less than current_line
+              previous_point = point
+            end 
 			
-			-- now create the transition point
-			local transition_time = current_line
-			local transition_value
+			if 
+              points[point].time > current_line and 
+              next_point > points[point].time 
+            then
+              -- get the lowest line which is greater than current_line
+              next_point = point				
+            end
+		
+          end
+			
+          -- now create the transition point
+          local transition_time = current_line
+          local transition_value
 
-			if previous_point == next_point then
-				transition_value = points[previous_point].value
-			else
-				transition_value = 
-					points[previous_point].value + 
-					(points[next_point].value - points[previous_point].value) *
-					(current_line - points[previous_point].time) /  
-					(points[next_point].time - points[previous_point].time)
-			end
+          if previous_point == next_point then
+            transition_value = points[previous_point].value
+          else
+            transition_value = 
+              points[previous_point].value + 
+              (points[next_point].value - points[previous_point].value) *
+              (current_line - points[previous_point].time) /  
+              (points[next_point].time - points[previous_point].time)
+          end
 					
-			new_pattern.tracks[num_track].automation[num_auto]:add_point_at(current_line,transition_value)
-			current_pattern.tracks[num_track].automation[num_auto]:add_point_at(current_line,transition_value)
+          new_pattern.tracks[num_track].automation[num_auto]:add_point_at(current_line,transition_value)
+          current_pattern.tracks[num_track].automation[num_auto]:add_point_at(current_line,transition_value)
 		
 		end
-		
-		
 		
 		-- delete any point which is before the current_line
-		for point = 1, table.getn(points) do
+        for point = 1, table.getn(points) do
 			
-			if points[point].time < current_line then
-				new_pattern.tracks[num_track].automation[num_auto]:remove_point_at(points[point].time)
-			end
+          if points[point].time < current_line then
+            new_pattern.tracks[num_track].automation[num_auto]:remove_point_at(points[point].time)
+          end
 			
-		end
+        end
 		
-		-- refresh copy after deletion	
-		points = new_pattern.tracks[num_track].automation[num_auto].points
+        -- refresh copy after deletion	
+        points = new_pattern.tracks[num_track].automation[num_auto].points
 		
-		-- shift back all the points after the current_line
-		for point = 1, table.getn(points) do
+        -- shift back all the points after the current_line
+        for point = 1, table.getn(points) do
 			
-			local auto_time = points[point].time - current_line + 1;
-			local auto_value = points[point].value
+          local auto_time = points[point].time - current_line + 1;
+          local auto_value = points[point].value
 			
-			new_pattern.tracks[num_track].automation[num_auto]:remove_point_at(points[point].time)			
-			new_pattern.tracks[num_track].automation[num_auto]:add_point_at(auto_time,auto_value)			
-			point = point - 1
-			
-		end
+          new_pattern.tracks[num_track].automation[num_auto]:remove_point_at(points[point].time)			
+          new_pattern.tracks[num_track].automation[num_auto]:add_point_at(auto_time,auto_value)			
+          point = point - 1
+		
+        end
 	
-	end
+    end
 	
   end
   
