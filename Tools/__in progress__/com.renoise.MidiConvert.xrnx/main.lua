f99bef632e016cebcf48ee4aa3db824175e1cad5
dac.chartrand <dac.chartrand@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-07-21 05:56:38 +0000

    Dummy main.lua file, forgot that it's mandatory and that Renoise complains if it's not found.

---
+++ Tools/__in progress__/com.renoise.MidiConvert.xrnx/main.lua
@@ -0,0 +1,13 @@
+
+-- Debug junk, work in progrsss
+-- return
+
+--[[
+require "Midi"
+local example = Midi()
+
+example:importMid("Scrapyard/test.midi")
+print(example:getTxt())
+-- rprint(example.tracks)
+-- example:saveMidFile("/Users/dac514/Desktop/test1.mid")
+]]--
