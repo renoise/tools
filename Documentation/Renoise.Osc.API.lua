d05bfca25765a6ec2039eb347c836ca291a3d3d5
mueller.eduard <mueller.eduard@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-08-05 11:02:52 +0000

    - more doc typo fixes

--- Documentation/Renoise.Osc.API.lua
+++ Documentation/Renoise.Osc.API.lua
@@ -1,12 +1,12 @@
 --[[============================================================================
-Renoise Socket API Reference
+Renoise OSC API Reference
 ============================================================================]]--
 
 --[[
 
 This reference describes the built-in OSC (Open Sound Control) support for 
 Lua scripts in Renoise. OSC can be used in combination with sockets to 
-send/receive Osc tagged data over process boundaries or exchange data 
+send/receive OSC tagged data over process boundaries or to exchange data 
 across computers in a network (Internet).
 
 Please have a look at http://opensoundcontrol.org for more general info 
@@ -20,8 +20,8 @@ Do not try to execute this file. It uses a .lua extension for markups only.
 
 -------- Examples
 
--- for some small examples on how to use the OSC and the sockets API, have a
--- look at the code sippets in the "Snippets/Osc.lua" file please. 
+-- For some small examples on how to use the OSC and sockets API, have a
+-- look at the code snippets in the "Snippets/Osc.lua" file please. 
 
 ]]
 
@@ -34,13 +34,12 @@ Do not try to execute this file. It uses a .lua extension for markups only.
 -- renoise.Osc
 --------------------------------------------------------------------------------
 
--- depackatizing raw (socket) data to OSC messages or bundles
-
--- converts the binary data to an OSC message or bundle. if the data does not 
+-- Depacketizing raw (socket) data to OSC messages or bundles
+-- converts the binary data to an OSC message or bundle. If the data does not 
 -- look like an OSC message, or the message contains errors, nil is returned 
 -- as first argument and the second return value will contain the error. 
--- if depackatizing was successfull, either a renoise.Osc.Bundle or Message 
--- object is returned. bundles may contain multiple messages or nested bundles.
+-- If depacketizing was successful, either a renoise.Osc.Bundle or Message 
+-- object is returned. Bundles may contain multiple messages or nested bundles.
 renoise.Osc.from_binary_data(binary_data) 
   -> [Osc.Bundle or Osc.Message object or nil, error or nil]
 
@@ -51,16 +50,15 @@ renoise.Osc.from_binary_data(binary_data)
 
 -------- create
 
--- create a new OSC message with the given pattern and optional arguments.
--- when arguments are specified, they must be specified as a table of 
+-- Create a new OSC message with the given pattern and optional arguments.
+-- When arguments are specified, they must be specified as a table of 
 -- {tag="X", value=SomeValue}'s. 
--- "tag" is the standard OSC type tag (see http://opensoundcontrol.org/spec-1_0)
--- all tags, except the 32 bit RGBA color tag are supported
--- "value" is the arguments value expressed by a Lua type. This value must 
--- be convertible to the specified tag, which means, you can for example 
--- not specify an "i" (integer) as type and pass a string as a value. Use a 
--- number value instead. Not all tags requre a value. the "value" field then
--- does not has to be specified.
+-- "tag" is a standard OSC type tag. (see http://opensoundcontrol.org/spec-1_0)
+-- "value" is the arguments value expressed by a Lua type. The value must 
+-- be convertible to the specified tag, which means, you cannot for example 
+-- specify an "i" (integer) as type and then pass a string as the value. Use a 
+-- number value instead. Not all tags require a value, like the T,F boolean tags. 
+-- Then a "value" field should not be specified.
 renoise.Osc.Message(pattern [, table of {tag, value} arguments])
 
 
@@ -70,28 +68,28 @@ renoise.Osc.Message(pattern [, table of {tag, value} arguments])
 message.pattern 
   -> [read-only, string]
 
--- table of {tag="X", value=SomeValue}'s that represent the message arguments.
--- see renoise.Osc.Message->create for more info
+-- table of {tag="X", value=SomeValue}'s that represents the message arguments.
+-- see renoise.Osc.Message "create" for more info.
 message.arguments 
   -> [read-only, table of {tag, value} tables]
   
--- raw binary representation of the messsage, as needed when sending the 
+-- raw binary representation of the messsage, as needed when e.g. sending the 
 -- message over the network through sockets
 message.binary_data 
   -> [read-only, raw string]
 
 
 --------------------------------------------------------------------------------
--- renoise.Socket.Bundle
+-- renoise.Osc.Bundle
 --------------------------------------------------------------------------------
 
 -------- create
 
--- create a new bundle by specifying a timetag and one or more messages.
--- if you don't know what to do with the timetag, simply use os.clock() 
--- which means "now". messages have to be renoise.Osc.Message objects. Nested
--- bundles (bundles with bundles) are right now not supported.
-renoise.Osc.Message(pattern, single_message_or_table_of_messages)
+-- Create a new bundle by specifying a timetag and one or more messages.
+-- If you do not know what to do with the timetag, use os.clock(),
+-- which simply means "now". Messages must be renoise.Osc.Message objects. 
+-- Nested bundles (bundles in bundles) are right now not supported.
+renoise.Osc.Bundle(pattern, single_message_or_table_of_messages)
 
 
 -------- properties
@@ -100,11 +98,11 @@ renoise.Osc.Message(pattern, single_message_or_table_of_messages)
 bundle.timetag
   -> [read-only, number]
   
--- access to the bundles elements (messages or again tables)
+-- access to the bundle elements (table of messages or bundle objects)
 bundle.elements
   -> [read-only, table of renoise.Osc.Message or renoise.Osc.Bundle objects]
   
--- raw binary representation of the bundle, as needed when sending the 
+-- raw binary representation of the bundle, as needed when e.g. sending the 
 -- message over the network through sockets
 bundle.binary_data 
   -> [read-only, raw string]
