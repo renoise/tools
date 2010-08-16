643055b6a4a0e73fab1b35563883e4e2ad2719eb
mueller.eduard <mueller.eduard@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-08-16 20:04:37 +0000

    duplex: - added KONTROL49 mappings (again thanks to beatslaughter)

---
+++ Tools/com.renoise.Duplex.xrnx/Duplex/Controllers/KONTROL49/KONTROL49.lua
@@ -0,0 +1,83 @@
+--[[----------------------------------------------------------------------------
+-- Duplex.KONTROL49
+----------------------------------------------------------------------------]]--
+
+-- default configuration of the KONTROL49
+-- uses a control map and the Mixer and Effect applications
+
+--==============================================================================
+
+-- setup a Mixer and Effect application
+
+duplex_configurations:insert {
+
+  -- configuration properties
+  name = "Mixer",
+  pinned = true,
+
+  -- device properties
+  device = {
+    display_name = "KONTROL49",
+    device_port_in = "MIDIIN2 (KONTROL49)",
+    device_port_out = "MIDIOUT2 (KONTROL49)",
+    control_map = "Controllers/KONTROL49/KONTROL49.xml",
+    thumbnail = "KONTROL49.bmp",
+    protocol = DEVICE_MIDI_PROTOCOL
+  },
+
+  applications = {
+    Mixer = {
+      mappings = {
+        mute = {
+          group_name = "Pads A"
+        },
+        solo = {
+          group_name = "Pads B"
+        },
+        panning = {
+          group_name = "Dials"
+        },
+        levels = {
+          group_name = "Faders"
+        },
+        page = {
+          group_name = "Switches"
+        }
+      },
+      options = {
+        pre_post = 2
+      }
+    }
+  }
+}
+
+duplex_configurations:insert {
+
+  -- configuration properties
+  name = "Effect",
+  pinned = true,
+
+  -- device properties
+  device = {
+    display_name = "KONTROL49",
+    device_port_in = "MIDIIN2 (KONTROL49)",
+    device_port_out = "MIDIOUT2 (KONTROL49)",
+    control_map = "Controllers/KONTROL49/KONTROL49.xml",
+    thumbnail = "KONTROL49.bmp",
+    protocol = DEVICE_MIDI_PROTOCOL
+  },
+
+  applications = {
+    Effect = {
+      mappings = {
+        parameters = {
+          group_name= "Dials"
+        },
+        page = {
+          group_name = "Switches"
+        }
+      }
+    }
+  }
+}
+
