02d76c722ad4841d6b155c7b8155fd40a7a0d9f6
mueller.eduard <mueller.eduard@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-08-18 15:07:00 +0000

    pattern rotate: - more ranges: "Whole Song", "Track in Song", - speed up processing by avoiding a scratch pattern (still a bit ugly, cause line properties now have to be copied manually) - some cosmetics

--- Tools/com.renoise.PatternRotate.xrnx/main.lua
+++ Tools/com.renoise.PatternRotate.xrnx/main.lua
@@ -2,14 +2,18 @@
 com.renoise.PatternRotate.xrnx/main.lua
 ============================================================================]]--
 
--- modes
+-- ranges
 
-local MODE_WHOLE_PATTERN = 1
-local MODE_TRACK_IN_PATTERN = 2
-local MODE_SELECTION_IN_PATTERN = 3
+local RANGE_WHOLE_SONG = 1
+local RANGE_WHOLE_PATTERN = 2
+local RANGE_TRACK_IN_SONG = 3
+local RANGE_TRACK_IN_PATTERN = 4
+local RANGE_SELECTION_IN_PATTERN = 5
 
-local mode_names = {
+local range_names = {
+  "Whole Song",
   "Whole Pattern",
+  "Track in Song",
   "Track in Pattern",
   "Selection in Pattern"
 }
@@ -20,9 +24,9 @@ local mode_names = {
 --------------------------------------------------------------------------------
 
 local preferences = renoise.Document.create {
-  mode = MODE_WHOLE_PATTERN,
-  amount = 1,
-  automation = true
+  range_mode = RANGE_WHOLE_PATTERN,
+  shift_amount = 1,
+  shift_automation = true
 }
 
 
@@ -30,45 +34,106 @@ local preferences = renoise.Document.create {
 -- processing helpers
 --------------------------------------------------------------------------------
 
--- moves and wraps an index into the specified range
+-- copy_effect_column:
+-- copy all effect column properties from src to dest column
 
-local function rotate_index(index, amount, range_start, range_end)
-  assert(index >= 0, "Internal error: unexpected rotate index")
-  assert(range_start <= range_end, "Internal error: unexpected rotate range")
-  
-  local range = range_end - range_start + 1
-  amount = amount % range
+local effect_column_properties = {
+  "number_value",
+  "amount_value",
+}
 
-  return (index - range_start + amount + range) % range + range_start
+local function copy_effect_column(src_column, dest_column)
+  for _,property in pairs(effect_column_properties) do
+    dest_column[property] = src_column[property]
+  end
 end
 
 
+-- copy_note_column:
+-- copy all note column properties from src to dest column
+
+local note_column_properties = {
+  "note_value",
+  "instrument_value",
+  "volume_value",
+  "panning_value",
+  "delay_value",
+}
+
+local function copy_note_column(src_column, dest_column)
+  for _,property in pairs(note_column_properties) do
+    dest_column[property] = src_column[property]
+  end
+end
+
+
+-- copy_line:
+-- creates a copy of the given patternline
+
+local function copy_line(src_line, dest_line)
+
+  for index,src_column in pairs(src_line.note_columns) do
+    if (not dest_line.note_columns[index]) then
+      dest_line.note_columns[index] = {}
+    end
+
+    local dest_column = dest_line.note_columns[index]
+    copy_note_column(src_column, dest_column)
+  end
+  
+  for index,src_column in pairs(src_line.effect_columns) do
+    if (not dest_line.effect_columns[index]) then
+      dest_line.effect_columns[index] = {}
+    end
+    
+    local dest_column = dest_line.effect_columns[index]
+    copy_effect_column(src_column, dest_column)
+  end
+end
+
+
+-- selection_line_range:
 -- queries the selection range start and end lines
 
-local function selection_line_range()
+local function selection_line_range(pattern_index)
 
   local line_start, line_end
   
-  local iter = renoise.song().pattern_iterator:lines_in_pattern(
-    renoise.song().selected_pattern_index)
-  
-  for pos,line in iter do
-    for _,note_column in pairs(line.note_columns) do
-      if (note_column.is_selected) then
-        line_start = line_start or pos.line
-        line_end = line_end and math.max(line_end, pos.line) or pos.line
-      end
-    end
+  if (pattern_index == renoise.song().selected_pattern_index) then
+    local iter = renoise.song().pattern_iterator:lines_in_pattern(pattern_index)
     
-    for _,effect_column in pairs(line.effect_columns) do
-      if (effect_column.is_selected) then
-        line_start = line_start or pos.line
-        line_end = line_end and math.max(line_end, pos.line) or pos.line
+    for pos,line in iter do
+      for _,note_column in pairs(line.note_columns) do
+        if (note_column.is_selected) then
+          line_start = line_start or pos.line
+          line_end = line_end and math.max(line_end, pos.line) or pos.line
+        end
+      end
+      
+      for _,effect_column in pairs(line.effect_columns) do
+        if (effect_column.is_selected) then
+          line_start = line_start or pos.line
+          line_end = line_end and math.max(line_end, pos.line) or pos.line
+        end
       end
     end
   end
-  
+    
   return line_start, line_end
+end
+
+
+-- rotate_index:
+-- shifts and wraps an index into a specified range
+
+local function rotate_index(index, shift_amount, range_start, range_end)
+  assert(index >= 0, "Internal error: unexpected rotate index")
+  assert(range_start <= range_end, "Internal error: invalid rotate range")
+  
+  local range = range_end - range_start + 1
+  shift_amount = shift_amount % range
+
+  return (index - range_start + shift_amount + range) % range + range_start
 end
 
 
@@ -76,130 +141,167 @@ end
 -- processing
 --------------------------------------------------------------------------------
 
--- rotate current pattern by the given amount in the given range
+-- rotate:
+-- current pattern by the given amount in the given range
 
-function rotate(amount, mode)
+function rotate(shift_amount, range_mode, shift_automation)
   
-  assert(type(amount) == "number", 
-    "Internal Error: Unexpected amount argument")
-  assert(type(mode) == "number" and mode >= 1 and mode <= #mode_names, 
-    "Internal Error: Unexpected mode argument")
+  range_mode = (range_mode ~= nil) and 
+    range_mode or preferences.range_mode.value
   
-  local song = renoise.song()
+  shift_automation = (shift_automation ~= nil) and 
+    shift_automation or preferences.shift_automation.value
   
-  local pattern = song.selected_pattern
-  local pattern_index = song.selected_pattern_index
+  assert(type(shift_amount) == "number", 
+    "Internal Error: Unexpected shift_amount argument")
+  assert(type(range_mode) == "number" and 
+    range_mode >= 1 and range_mode <= #range_names, 
+    "Internal Error: Unexpected range_mode argument")
+  assert(type(shift_automation) == "boolean" and 
+    "Internal Error: Unexpected shift_automation argument")
   
+  local patterns = renoise.song().patterns
+  local tracks = renoise.song().tracks
   
-  -- create a scratch pattern 
+  local selected_track_index = renoise.song().selected_track_index
+  local selected_pattern_index = renoise.song().selected_pattern_index
+    
+  
+  ----- get the processing pattern & track range
 
-  local temp_pattern_index = song.sequencer:insert_new_pattern_at(
-    #song.sequencer.pattern_sequence)
-  
-  local temp_pattern = song.patterns[temp_pattern_index]
+  local process_selection = (range_mode == RANGE_SELECTION_IN_PATTERN)
 
-  if (not temp_pattern.is_empty) then 
-    -- hackaround for a bug in b4: 
-    -- insert_new_pattern_at returns the previous pattern
-    temp_pattern_index = temp_pattern_index + 1
-    temp_pattern = song.patterns[temp_pattern_index]
-  end
+  local pattern_start, pattern_end
+  local track_start, track_end
   
+  if (range_mode == RANGE_WHOLE_SONG) then
+    pattern_start, pattern_end = 1, #patterns
+    track_start, track_end = 1, #tracks
   
-  -- collect to be processed tracks
-
-  local tracks_to_process = nil
+  elseif (range_mode == RANGE_WHOLE_PATTERN) then
+    pattern_start, pattern_end = selected_pattern_index, selected_pattern_index
+    track_start, track_end = 1, #tracks
   
-  if (mode == MODE_WHOLE_PATTERN) then
-    tracks_to_process = pattern.tracks
-  elseif (mode == MODE_SELECTION_IN_PATTERN) then
-    tracks_to_process = pattern.tracks 
-  elseif (mode == MODE_TRACK_IN_PATTERN) then
-    tracks_to_process = {
-      [song.selected_track_index] = song.selected_pattern_track
-    }
+  elseif (range_mode == RANGE_TRACK_IN_SONG) then
+    pattern_start, pattern_end = 1, #patterns
+    track_start, track_end = selected_track_index, selected_track_index
+  
+  elseif (range_mode == RANGE_TRACK_IN_PATTERN) then
+    pattern_start, pattern_end = selected_pattern_index, selected_pattern_index
+    track_start, track_end = selected_track_index, selected_track_index
+  
+  elseif (range_mode == RANGE_SELECTION_IN_PATTERN) then
+    pattern_start, pattern_end = selected_pattern_index, selected_pattern_index
+    track_start, track_end = 1, #tracks
+  
   else
     error("Internal error: unexpected rotate range mode")
   end
-
-
-  -- get the selection line range
-
-  local selection_start, selection_end, selection_range
   
-  if (mode == MODE_SELECTION_IN_PATTERN) then
-    selection_start, selection_end = selection_line_range()
   
-    if (selection_start and selection_end) then
-      selection_range = selection_end - selection_start + 1
+  ----- rotate each pattern in the processing range
+          
+  for pattern_index = pattern_start,pattern_end do
+    local pattern = patterns[pattern_index] 
+      
+    -- get the processing line range for the pattern
+    local line_start, line_end, line_range
+    
+    if (process_selection) then
+      line_start, line_end = selection_line_range(pattern_index)
+    else
+      line_start, line_end = 1, pattern.number_of_lines
     end
-  end
-        
-        
-  -- rotate track by track
-          
-  for track_index,track in pairs(tracks_to_process) do
-    local temp_track = temp_pattern.tracks[track_index]
+       
+    if (line_start and line_end) then
+      line_range = line_end - line_start + 1
+    else
+      -- no lines to rotate. bail out...
+      break   
+    end    
+  
+  
+    ---- rotate each track in the pattern in the processing range
     
-    -- copy the existing lines to the scratch track
-    -- pattern lines are references. can't move them in-place
-    temp_track:copy_from(track)
-    
-    -- handle selection mode seperately
-    if (mode == MODE_SELECTION_IN_PATTERN) then
-      if (selection_range) then
+    for track_index = track_start,track_end do
+      local track = pattern.tracks[track_index] 
+      
+      -- copy relevant lines to a temp array first (pattern lines are references)
 
-        -- paste selected columns back to the new pos
-        for line_index = selection_start,selection_end do
-          
-          local dest_line_index = rotate_index(line_index, 
-            amount, selection_start, selection_end)
-
-          local src_line = temp_track:line(line_index)
-          local dest_line = track:line(dest_line_index)
-          
+      local temp_lines = {}
+  
+      for line_index = line_start,line_end do
+        local src_line = track:line(line_index)
+        
+        if (process_selection or not src_line.is_empty) then
+        
+          -- will copy later on, empty or not...
+          temp_lines[line_index] = { 
+            is_empty = src_line.is_empty, 
+            note_columns = {}, 
+            effect_columns = {} 
+          }
+  
+          copy_line(src_line, temp_lines[line_index])
+        
+        else
+  
+          -- will skip those lines later on...
+          temp_lines[line_index] = { is_empty = true }
+        end
+      end
+      
+      
+      -- rotate pattern lines or selected columns
+      
+      for line_index = line_start,line_end do
+        local dest_line_index = rotate_index(line_index, 
+          shift_amount, line_start, line_end)
+  
+        local src_line = temp_lines[line_index]
+        local dest_line = track:line(dest_line_index)
+        
+        if (process_selection) then
+          -- copy column by column, checking the selection state
           for index,note_column in pairs(src_line.note_columns) do
             local dest_note_column = dest_line.note_columns[index]
             
             if (dest_note_column.is_selected) then
-              dest_note_column:copy_from(note_column)
+              copy_note_column(note_column, dest_note_column)
             end
           end
           
           for index,effect_column in pairs(src_line.effect_columns) do
             local dest_effect_column = dest_line.effect_columns[index]
-
+            
             if (dest_effect_column.is_selected) then
-              dest_effect_column:copy_from(effect_column)
+              copy_effect_column(effect_column, dest_effect_column)
             end
+          end
+        
+        else
+          -- copy whole lines or clear in one batch (just to speed up things)
+          if (src_line.is_empty) then
+            dest_line:clear()
+          else
+            copy_line(src_line, dest_line)
           end
         end
       end
+                
+                
+      -- rotate automation but (not for pattern selections)
       
-    else
-      -- paste lines from the temp track to the new pos
-      local number_of_lines = pattern.number_of_lines
-      
-      for line_index = 1,number_of_lines do
-        
-        local dest_line_index = rotate_index(line_index, 
-          amount, 1, number_of_lines)
-        
-        local src_line = temp_track:line(line_index)
-        local dest_line = track:line(dest_line_index)
-        
-        dest_line:copy_from(src_line)
-      end
-      
-      -- rotate automation in place
-      if (preferences.automation.value == true) then
+      if (shift_automation and not process_selection) then
         for _,automation in pairs(track.automation) do
           local rotated_points = table.create(
             table.rcopy(automation.points))
           
           for _,point in pairs(rotated_points) do
-            point.time = rotate_index(point.time, 
-              amount, 1, number_of_lines)
+            if (point.time >= line_start and point.time <= line_end) then
+              point.time = rotate_index(point.time, 
+                shift_amount, line_start, line_end)
+            end
           end
           
           rotated_points:sort(function(a,b) 
@@ -211,12 +313,6 @@ function rotate(amount, mode)
       end
     end
   end
-  
-  
-  -- delete the scratch pattern
-  
-  temp_pattern:clear() -- empty patterns will be completely removed
-  song.sequencer:delete_sequence_at(#song.sequencer.pattern_sequence)
 end
 
 
@@ -260,16 +356,15 @@ function show_dialog()
     -- Mode
     vb:column {
       vb:text {
-        width = 30,
         text = "What:" 
       },
       vb:popup {
         width = POPUP_WIDTH,
-        items = mode_names, 
-        bind = preferences.mode,
+        items = range_names, 
+        bind = preferences.range_mode,
         notifier = function()
           vb.views.automation_column.visible = 
-            (preferences.mode.value ~= MODE_SELECTION_IN_PATTERN)
+            (preferences.range_mode.value ~= RANGE_SELECTION_IN_PATTERN)
           
           vb.views.dialog_content:resize()
         end 
@@ -279,28 +374,26 @@ function show_dialog()
     -- Amount
     vb:column {
       vb:text { 
-        width = 30,
         text = "By Lines:"  
       },
       vb:row { 
         vb:valuebox { 
           min = 1, 
           max = 256,
-          bind = preferences.amount
+          bind = preferences.shift_amount
         }
       }
     },
     
-    
     -- Rotate Automation
     vb:column {
       id = "automation_column",
-      visible = (preferences.mode.value ~= MODE_SELECTION_IN_PATTERN), 
-
+      visible = (preferences.range_mode.value ~= RANGE_SELECTION_IN_PATTERN), 
+      
       vb:space { height = 2*DEFAULT_SPACING },
       
       vb:row {
-        vb:checkbox { bind = preferences.automation },
+        vb:checkbox { bind = preferences.shift_automation },
         vb:text { text = "Rotate Automation" }
       }
     },
@@ -314,7 +407,7 @@ function show_dialog()
         width = BUTTON_WIDTH, 
         height = BUTTON_HEIGHT,
         notifier = function()
-          rotate(-preferences.amount.value, preferences.mode.value)
+          rotate(-preferences.shift_amount.value)
         end
       },
       vb:button { 
@@ -322,7 +415,7 @@ function show_dialog()
         width = BUTTON_WIDTH, 
         height = BUTTON_HEIGHT,
         notifier = function()
-          rotate(preferences.amount.value, preferences.mode.value)
+          rotate(preferences.shift_amount.value)
         end
       }
     }
@@ -338,10 +431,10 @@ function show_dialog()
       dialog:close()        
     
     elseif (key.name == "up") then
-      rotate(-preferences.amount.value, preferences.mode.value)
+      rotate(-preferences.shift_amount.value)
     
     elseif (key.name == "down") then
-      rotate(preferences.amount.value, preferences.mode.value)
+      rotate(preferences.shift_amount.value)
     
     elseif (key.name == "tab") then
       local song = renoise.song()
@@ -399,22 +492,22 @@ renoise.tool():add_keybinding {
 
 renoise.tool():add_keybinding {
   name = "Pattern Editor:Pattern Operations:Rotate Pattern up",
-  invoke = function() rotate(-1, MODE_WHOLE_PATTERN) end
+  invoke = function() rotate(-1, RANGE_WHOLE_PATTERN) end
 }
 
 renoise.tool():add_keybinding {
   name = "Pattern Editor:Pattern Operations:Rotate Pattern down",
- invoke = function() rotate(1, MODE_WHOLE_PATTERN) end
+ invoke = function() rotate(1, RANGE_WHOLE_PATTERN) end
 }
 
 renoise.tool():add_keybinding {
   name = "Pattern Editor:Selection:Rotate Selection up",
-  invoke = function() rotate(-1, MODE_SELECTION_IN_PATTERN) end
+  invoke = function() rotate(-1, RANGE_SELECTION_IN_PATTERN) end
 }
 
 renoise.tool():add_keybinding {
   name = "Pattern Editor:Selection:Rotate Selection down",
- invoke = function() rotate(1, MODE_SELECTION_IN_PATTERN) end
+  invoke = function() rotate(1, RANGE_SELECTION_IN_PATTERN) end
 }
 
 
