622cdda62a7abc191805b7a3eeb4b0e5c3d0d1c8
mueller.eduard <mueller.eduard@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-08-24 09:13:10 +0000

    pattern rotate: - moved pattern line tools (copy lines, selection helpers) into a new file to make things a bit more easy and reusable

--- Tools/com.renoise.PatternRotate.xrnx/main.lua
+++ Tools/com.renoise.PatternRotate.xrnx/main.lua
@@ -2,6 +2,11 @@
 com.renoise.PatternRotate.xrnx/main.lua
 ============================================================================]]--
 
+-- requires
+
+require "pattern_line_tools"
+
+
 -- ranges
 
 local RANGE_WHOLE_SONG = 1
@@ -31,99 +36,9 @@ local preferences = renoise.Document.create {
 
 
 --------------------------------------------------------------------------------
--- processing helpers
+-- local tools
 --------------------------------------------------------------------------------
 
--- copy_effect_column:
--- copy all effect column properties from src to dest column
-
-local effect_column_properties = {
-  "number_value",
-  "amount_value",
-}
-
-local function copy_effect_column(src_column, dest_column)
-  for _,property in pairs(effect_column_properties) do
-    dest_column[property] = src_column[property]
-  end
-end
-
-
--- copy_note_column:
--- copy all note column properties from src to dest column
-
-local note_column_properties = {
-  "note_value",
-  "instrument_value",
-  "volume_value",
-  "panning_value",
-  "delay_value",
-}
-
-local function copy_note_column(src_column, dest_column)
-  for _,property in pairs(note_column_properties) do
-    dest_column[property] = src_column[property]
-  end
-end
-
-
--- copy_line:
--- creates a copy of the given patternline
-
-local function copy_line(src_line, dest_line)
-
-  for index,src_column in pairs(src_line.note_columns) do
-    if (not dest_line.note_columns[index]) then
-      dest_line.note_columns[index] = {}
-    end
-
-    local dest_column = dest_line.note_columns[index]
-    copy_note_column(src_column, dest_column)
-  end
-  
-  for index,src_column in pairs(src_line.effect_columns) do
-    if (not dest_line.effect_columns[index]) then
-      dest_line.effect_columns[index] = {}
-    end
-    
-    local dest_column = dest_line.effect_columns[index]
-    copy_effect_column(src_column, dest_column)
-  end
-end
-
-
--- selection_line_range:
--- queries the selection range start and end lines
-
-local function selection_line_range(pattern_index)
-
-  local line_start, line_end
-  
-  if (pattern_index == renoise.song().selected_pattern_index) then
-    local iter = renoise.song().pattern_iterator:lines_in_pattern(pattern_index)
-    
-    for pos,line in iter do
-      for _,note_column in pairs(line.note_columns) do
-        if (note_column.is_selected) then
-          line_start = line_start or pos.line
-          line_end = line_end and math.max(line_end, pos.line) or pos.line
-        end
-      end
-      
-      for _,effect_column in pairs(line.effect_columns) do
-        if (effect_column.is_selected) then
-          line_start = line_start or pos.line
-          line_end = line_end and math.max(line_end, pos.line) or pos.line
-        end
-      end
-    end
-  end
-    
-  return line_start, line_end
-end
-
-
--- rotate_index:
 -- shifts and wraps an index into a specified range
 
 local function rotate_index(index, shift_amount, range_start, range_end)
@@ -137,12 +52,12 @@ local function rotate_index(index, shift_amount, range_start, range_end)
 end
 
 
+
 --------------------------------------------------------------------------------
 -- processing
 --------------------------------------------------------------------------------
 
--- rotate:
--- current pattern by the given amount in the given range
+-- rotate patterns in the specified range by the given amount
 
 function rotate(shift_amount, range_mode, shift_automation)
   
@@ -290,7 +205,7 @@ function rotate(shift_amount, range_mode, shift_automation)
       end
                 
                 
-      -- rotate automation but (not for pattern selections)
+      -- rotate automation (not for pattern selections)
       
       if (shift_automation and not process_selection) then
         for _,automation in pairs(track.automation) do
