9973b3379020af1261f74628cb99a8fad4ceb528
mueller.eduard <mueller.eduard@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-10-01 14:48:03 +0000

    - added docs and examples for renoise.tool().app_saved_document_observable for B7

--- trunk/Documentation/Renoise.ScriptingTool.API.lua
+++ trunk/Documentation/Renoise.ScriptingTool.API.lua
@@ -236,13 +236,6 @@ renoise.tool().app_resigned_active_observable
 renoise.tool().app_idle_observable
   -> [renoise.Document.Observable object]
 
--- invoked each time a new document (song) was created or loaded, aka each time
--- the result of renoise.song() has changed. also called when the script gets
--- reloaded (only happens with the auto_reload debugging tools), in order 
--- to connect the new script instance to the already running document.
-renoise.tool().app_new_document_observable
-  -> [renoise.Document.Observable object]
-
 -- invoked each time before a new document gets created or loaded, aka the last 
 -- time renoise.song() still points to the old song before a new one arrives.
 -- you can explicitely release notifiers to the old document here, or do some own
@@ -250,6 +243,17 @@ renoise.tool().app_new_document_observable
 [added B7] renoise.tool().app_release_document_observable
   -> [renoise.Document.Observable object]
 
+-- invoked each time a new document (song) was created or loaded, aka each time
+-- the result of renoise.song() has changed. also called when the script gets
+-- reloaded (only happens with the auto_reload debugging tools), in order 
+-- to connect the new script instance to the already running document.
+renoise.tool().app_new_document_observable
+  -> [renoise.Document.Observable object]
+
+-- invoked each time the apps document (song) got successfully saved. 
+-- renoise.song().file_name will point to the filename it got saved to.
+[added B7] renoise.tool().app_saved_document_observable
+  -> [renoise.Document.Observable object]
 
 -- get or set an optional renoise.Document.DocumentNode object, which will be
 -- used as set of persistant "options" or preferences for your tool. 
