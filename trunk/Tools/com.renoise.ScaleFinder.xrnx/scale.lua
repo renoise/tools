cd1b8448eec41902d47b015a929d822b2286450f
ClySuva <ClySuva@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-10-12 14:35:00 +0000

    + adv

--- trunk/Tools/com.renoise.ScaleFinder.xrnx/scale.lua
+++ trunk/Tools/com.renoise.ScaleFinder.xrnx/scale.lua
@@ -1,10 +1,10 @@
 notes = { 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#' };
 scales = {
-	major = {
+	maj = {
 		name = 'Major',
 		pattern = "101011010101"
 	},
-	minor = {
+	min = {
 		name = 'Minor',
 		pattern = '101101011010'
 	}
@@ -36,35 +36,34 @@ chords = {
 
 
 ------------------------------------------------------
-local root  = 2
-local scale = scales['major']
-
-local spat = scale['pattern'];
-local rpat = {false,false,false,false,false,false,false,false,false,false,false}
--- Finds scale notes
-for i = 0,11 do
-	local note = ((root - 1 + i) % 12) + 1
-	if spat:sub(i+1, i+1) == '1' then
-		rpat[note] = true
+function get_scale(root, scale)
+	local spat = scale['pattern'];
+	local rpat = {false,false,false,false,false,false,false,false,false,false,false}
+	-- Finds scale notes
+	for i = 0,11 do
+		local note = ((root - 1 + i) % 12) + 1
+		if spat:sub(i+1, i+1) == '1' then
+			rpat[note] = true
+		end
 	end
+	return rpat
 end
 
 ------------------------------------------------------
-
-
-local chord = chords['dim']
-local root = 1
-local scale_pattern = rpat
-
-local cpat = chord['pattern'];
-for i = 0,11 do
-	local note = ((root - 1 + i) % 12) + 1
-	if cpat:sub(i+1, i+1) == '1' then
-		print(i)
-		print(notes[note])
-		if not scale_pattern[note] then
-			print('doesnt match!')
+function is_valid(root, chord, scale_pattern)
+	local cpat = chord['pattern']
+	for i = 0,11 do
+		local note = ((root - 1 + i) % 12) + 1
+		if cpat:sub(i+1, i+1) == '1' then
+			if not scale_pattern[note] then
+				return false;
+			end
 		end
 	end
+	return true;
 end
 
+
+sp = get_scale(1, scales['maj'])
+
+print(is_valid(1, chords['min'], sp))
