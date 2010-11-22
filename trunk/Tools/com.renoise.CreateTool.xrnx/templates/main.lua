529f09476f8132a697fa2436e4604c88b514d521
marvintjon <marvintjon@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-11-22 15:58:59 +0000

    First public release, includes fix for description textarea and a basic template.

--- trunk/Tools/com.renoise.CreateTool.xrnx/templates/main.lua
+++ trunk/Tools/com.renoise.CreateTool.xrnx/templates/main.lua
@@ -1,3 +1,71 @@
--------------------------------------
--- Main.lua
--------------------------------------
+--[[============================================================================
+main.lua
+============================================================================]]--
+
+-- internal state
+
+local dialog = nil
+local vb = nil
+
+_AUTO_RELOAD_DEBUG = function()
+  dialog = nil
+  vb = nil
+end
+
+-- Get Tool Name
+
+class "RenoiseScriptingTool" (renoise.Document.DocumentNode)
+  function RenoiseScriptingTool:__init()    
+    renoise.Document.DocumentNode.__init(self) 
+    self:add_property("Name", "Untitled Tool")
+  end
+
+local manifest = RenoiseScriptingTool()
+local ok,err = manifest:load_from("manifest.xml")
+local tool_name = manifest:property("Name").value
+
+
+--------------------------------------------------------------------------------
+-- Main
+--------------------------------------------------------------------------------
+
+local function get_greeting()
+  local words = {"Hello world!", "Nice to meet you :)", "Hi there!"}
+  local id = math.random(#words)
+  return words[id]
+end
+
+
+--------------------------------------------------------------------------------
+-- GUI
+--------------------------------------------------------------------------------
+
+local function show_dialog()
+  if dialog and dialog.visible then
+    dialog:show()
+    return
+  end
+  
+  vb = renoise.ViewBuilder()
+  
+  local content = vb:column {
+    margin = 10,
+    vb:text {
+      text = get_greeting()
+    }
+  } 
+  
+  dialog = renoise.app():show_custom_dialog(tool_name, content)  
+end
+
+
+--------------------------------------------------------------------------------
+-- Menu entries
+--------------------------------------------------------------------------------
+
+renoise.tool():add_menu_entry {
+  name = "Main Menu:Tools:"..tool_name.."...",
+  invoke = function()
+    show_dialog()
+  end
+}
