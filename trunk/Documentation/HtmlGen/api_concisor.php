ff09038d893e382524321584e1468d127079e2c6
dac.chartrand@gmail.com <dac.chartrand@gmail.com@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2010-12-30 22:11:02 +0000

    Finished work on api_concisor; is now functional.

--- trunk/Documentation/HtmlGen/api_concisor.php
+++ trunk/Documentation/HtmlGen/api_concisor.php
@@ -10,20 +10,65 @@ foreach(new DirectoryIterator($CONFIG['DOCS_DIR']) as $file) {
 
     $tmp = file_get_contents($file->getPathname());
 
+    /*
+    Regular expression mayhem
+    For modifier explinations, see:
+    http://php.net/manual/en/reference.pcre.pattern.modifiers.php
+    */
+
+    // Find `--[[ % ]]--` or `--[[ % ]]` and replace with null
     $tmp = preg_replace('/--\[\[(.*?)(\]\]--|\]\])/s', null, $tmp);
+
+    // Find `-- %` and replace with null
     $tmp = preg_replace('/^--(.*?)$/sm', null, $tmp);
-    $tmp = preg_replace('/\s*?->(.*?)$/sm', null, $tmp);
+
+    // Find `_observable` and replace with null
     $tmp = str_replace(', _observable', null, $tmp);
+    $tmp = str_replace(', TODO: _observable', null, $tmp);
 
-    $tmp = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $tmp);
-
-    // $concise .= "\n >>> " . $file->getFilename() . " <<< \n";
-
+    // Append to array
     $concise = array_merge($concise, explode("\n", $tmp));
 }
 
-// natcasesort($concise);
+foreach ($concise as $key => &$val) {
 
-print_r($concise);
+    $val = rtrim($val);
+
+    // If `(` or ` or` are the last characters, concatenate the next line
+    if (preg_match('/(\(| or)$/', $val)) {
+        $val .= @trim($concise[$key+1]);
+        unset($concise[$key+1]);
+    }
+
+    // Remove everything after ` -> `, also check the next line...
+    if (preg_match('/\s*?->(.*?)/', $val)) {
+        $val = preg_replace('/\s*?->(.*?)$/', null, $val);
+        // Next line...
+        if (preg_match('/^\s.?/', @$concise[$key+1])) {
+            unset($concise[$key+1]);
+        }
+    }
+
+    $val = trim($val);
+
+    // Remove blank lines
+    if ($val == '') {
+        unset($concise[$key]);
+        continue;
+    }
+
+    // Remove operators
+    if (!preg_match('/^[A-z0-9]/', $val)) {
+        unset($concise[$key]);
+        continue;
+    }
+
+}
+
+// Sort and print
+natcasesort($concise);
+$concise = array_values($concise);
+// print_r($concise);
+echo implode("\n", $concise) . "\n";
 
 ?>
