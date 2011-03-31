75e28991c632fc52cbd2bc7ab3ea70e3284ebca1
marvintjon <marvintjon@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2011-03-31 11:42:39 +0000

    Fixed chunked transfer encoding, streaming files to disk

--- Libraries/renoise/http/util.lua
+++ Libraries/renoise/http/util.lua
@@ -15,11 +15,23 @@
 require "renoise.http.url"
 
 
+function os.capture(cmd, raw)
+  local f = assert(io.popen(cmd, 'r'))
+  local s = assert(f:read('*a'))
+  f:close()
+  if raw then return s end
+  s = string.gsub(s, '^%s+', '')
+  s = string.gsub(s, '%s+$', '')
+  s = string.gsub(s, '[\n\r]+', ' ')
+  return s
+end
+
 -------------------------------------------------------------------------------
 --  Util class
 -------------------------------------------------------------------------------
-
 class 'Util'
+
+Util.CLRF = "\r\n"
 
 function Util:split_lines(str)
   local t = {}
