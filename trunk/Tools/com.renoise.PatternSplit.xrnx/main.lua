609e23bdc313b58287e5ed0a0879ef9473878938
mueller.eduard <mueller.eduard@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2011-05-02 13:45:50 +0000

    - updated com.renoise.PatternSplit.xrnx for Renoise 2.7 - few optimizations (more locals, avoid creating too many temp tables)

--- trunk/Tools/com.renoise.PatternSplit.xrnx/main.lua
+++ trunk/Tools/com.renoise.PatternSplit.xrnx/main.lua
@@ -27,57 +27,64 @@ renoise.tool():add_keybinding {
 
 function split()
 
-  local selected_line = renoise.song().selected_line;
-
+  --get the song that currently is active
+  local song = renoise.song()
+  
   --get the ordinal number of the current line (where to split)
-  local current_line = renoise.song().transport.edit_pos.line;
+  local current_line = song.transport.edit_pos.line
 
   --get the current pattern    
-  local current_pattern = renoise.song().selected_pattern;
+  local current_pattern = song.selected_pattern
   
   
   if current_line == current_pattern.number_of_lines then
-    renoise.app():show_status('Warning: cannot split at the bottom of the pattern, operation aborted.');
+    renoise.app():show_status(
+      'Warning: cannot split at the bottom of the pattern, operation aborted.')
     return
   end
 
   --clone current pattern
-  local new_pattern_index =    
-    renoise.song().sequencer:clone_range(
-      renoise.song().selected_sequence_index,
-      renoise.song().selected_sequence_index);
+  local selected_sequence_index = 
+    song.selected_sequence_index
+  
+  local new_pattern_index = song.sequencer:clone_range(
+    selected_sequence_index, selected_sequence_index)
     
   --get the new pattern  
-  local new_pattern = renoise.song().patterns[
-    renoise.song().sequencer.pattern_sequence[
-      renoise.song().selected_sequence_index+1]];
+  local new_pattern = song:pattern(
+    song.sequencer:pattern(selected_sequence_index + 1))
       
   --cut the current pattern at that line
-  current_pattern.number_of_lines = current_line; 
+  current_pattern.number_of_lines = current_line 
   
-  local num_tracks = table.getn(new_pattern.tracks);
+  local num_tracks = #new_pattern.tracks
 
   -- dealing with automation.
-  -- first of all, let's create an intermediate point between the last point of previous
-  -- pattern and the first of the new pattern, if it doesn't exists yet
+  -- first of all, let's create an intermediate point between the last point of
+  -- previous pattern and the first of the new pattern, if it doesn't exists yet
   for num_track = 1, num_tracks do
   
-    for num_auto = 1, table.getn(new_pattern.tracks[num_track].automation) do
+    local current_track = current_pattern.tracks[num_track]
+    local new_track = new_pattern:track(num_track)
+    
+    for num_auto = 1, #new_track.automation do
 
-      local points = new_pattern.tracks[num_track].automation[num_auto].points
+      local current_automation = current_track.automation[num_auto]
+      local new_automation = new_track.automation[num_auto]
+      local points = new_automation.points
     
       -- let's see if the line on which we are cutting already has a point
       -- in the curve; in such case, we are lucky and don't need to compute
       -- the intermediate point..
       if not 
-        new_pattern.tracks[num_track].automation[num_auto]:has_point_at(current_line) 
+        new_automation:has_point_at(current_line) 
       then
         -- let's see if the current automation curve has any point before the
         -- current_line and any point after current_line.
         local previous_point = 1
-        local next_point = table.getn(points)
+        local next_point = #points
 
-        for point = 1, table.getn(points) do
+        for point = 1, #points do
         
           if 
             points[point].time < current_line and 
@@ -104,48 +111,49 @@ function split()
         if previous_point == next_point then
           transition_value = points[previous_point].value
         else
-          transition_value = 
-            points[previous_point].value + 
+          transition_value = points[previous_point].value + 
             (points[next_point].value - points[previous_point].value) *
-            (current_line - points[previous_point].time) /  
-            (points[next_point].time - points[previous_point].time)
+              (current_line - points[previous_point].time) /  
+                (points[next_point].time - points[previous_point].time)
         end
         
-        new_pattern.tracks[num_track].automation[num_auto]:add_point_at(current_line+1,transition_value)
-        current_pattern.tracks[num_track].automation[num_auto]:add_point_at(current_line,transition_value)
+        new_automation:add_point_at(current_line+1,transition_value)
+        current_automation:add_point_at(current_line,transition_value)
 
       else
 
-        for point = 1, table.getn(points) do
+        for point = 1, #points do
+        
           if points[point].time == current_line then
-            new_pattern.tracks[num_track].automation[num_auto]:add_point_at(current_line+1,points[point].value)
-            point = table.getn(points) -- break
+            new_automation:add_point_at(current_line+1,points[point].value)
+            point = #points -- break
           end
+          
         end
 
       end
 
       -- delete any point which is before the current_line
-      for point = 1, table.getn(points) do
+      for point = 1, #points do
       
         if points[point].time < current_line + 1 then
-          new_pattern.tracks[num_track].automation[num_auto]:remove_point_at(points[point].time)
+          new_automation:remove_point_at(points[point].time)
           point = point - 1
         end
     
       end
     
       -- refresh copy after deletion  
-      points = new_pattern.tracks[num_track].automation[num_auto].points
+      points = new_automation.points
     
       -- shift back all the points after the current_line
-      for point = 1, table.getn(points) do
+      for point = 1, #points do
 
         local auto_time = points[point].time - current_line
         local auto_value = points[point].value
 
-        new_pattern.tracks[num_track].automation[num_auto]:remove_point_at(points[point].time)      
-        new_pattern.tracks[num_track].automation[num_auto]:add_point_at(auto_time,auto_value)
+        new_automation:remove_point_at(points[point].time)      
+        new_automation:add_point_at(auto_time,auto_value)
         
       end
   
@@ -155,16 +163,18 @@ function split()
   
   --iterate through tracks of the new pattern to shift lines up
   for num_track = 1, num_tracks do
+  
+    local new_track = new_pattern:track(num_track)
 
     for i = current_line+1,new_pattern.number_of_lines do
-      new_pattern.tracks[num_track]:line(i - current_line):copy_from(
-        new_pattern.tracks[num_track]:line(i));
+      new_track:line(i - current_line):copy_from(new_track:line(i))
     end 
   
   end
 
   -- cut the new pattern
-  new_pattern.number_of_lines = new_pattern.number_of_lines - current_line;
+  new_pattern.number_of_lines = 
+    new_pattern.number_of_lines - current_line
  
 end
 
