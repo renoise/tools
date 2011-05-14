a254779432291df6ca7509239ed3c2db7ac5ae7f
dac.chartrand@gmail.com <dac.chartrand@gmail.com@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2011-05-14 15:39:56 +0000

    Echo an "OK" message if things seem fine.

--- Xtra/ToolScanner/run.php
+++ Xtra/ToolScanner/run.php
@@ -4,16 +4,21 @@
 // Example
 // ----------------------------------------------------------------------------
 
-$file = '/Users/dac514/Desktop/com.renoise.IRCclientV1_13.xrnx';
+$file = '~/Downloads/foo.xrnx';
 
 require('scanner.php');
 
 if (file_exists(@$argv[1])) $file = $argv[1];
 $result = scan($file);
+
+if (!headers_sent()) header('Content-Type: text/plain');
+
 if ($result !== true) {
-    if (!headers_sent()) header('Content-Type: text/plain');
     echo "Warnings Found: " . count($result) . "\n";
     print_r($result);
+}
+else {
+    echo "Nothing suspicious found. Everything seems OK. \n";
 }
 
 ?>
