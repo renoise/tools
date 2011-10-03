b8dd2064dc236c19dd5a898df1c8643f3bef3abc
dac.chartrand@gmail.com <dac.chartrand@gmail.com@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2011-10-03 02:02:31 +0000

    Grid Pie v0.3

--- trunk/Tools/com.renoise.GridPie.xrnx/toolbox.lua
+++ trunk/Tools/com.renoise.GridPie.xrnx/toolbox.lua
@@ -1,3 +1,4 @@
+--[[ Math ]]--
 
 -- greatest common divisor
 function gcd(m,n)
@@ -21,4 +22,25 @@ function least_common(...)
     cm = lcm(cm,arg[i+1])
   end
   return cm
+end
+
+
+--[[ Midi ]]--
+
+function midi_debug(message)
+
+  print(("  message:is_trigger(): %s)"):format(
+    message:is_trigger() and "yes" or "no"))
+  print(("  message:is_switch(): %s)"):format(
+    message:is_switch() and "yes" or "no"))
+  print(("  message:is_rel_value(): %s)"):format(
+    message:is_rel_value() and "yes" or "no"))
+  print(("  message:is_abs_value(): %s)"):format(
+    message:is_abs_value() and "yes" or "no"))
+  print(("  message.int_value: %d)"):format(
+    message.int_value))
+  print(("  message.boolean_value: %s)"):format(
+    message.boolean_value and "true" or "false"))
+  print("---------------------------------------------------------------------")
+
 end
