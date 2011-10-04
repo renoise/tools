e47361222b8cb9e3c8a1b4dec51a26812547baef
dac.chartrand@gmail.com <dac.chartrand@gmail.com@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2011-10-04 14:58:34 +0000

    Grid Pie v0.4 Uses OneShotIdleNotifier() class to hack around procedures that weren't possible before.

--- trunk/Tools/com.renoise.GridPie.xrnx/toolbox.lua
+++ trunk/Tools/com.renoise.GridPie.xrnx/toolbox.lua
@@ -29,6 +29,7 @@ end
 
 function midi_debug(message)
 
+  print("--------------------------------------")
   print(("  message:is_trigger(): %s)"):format(
     message:is_trigger() and "yes" or "no"))
   print(("  message:is_switch(): %s)"):format(
@@ -41,6 +42,35 @@ function midi_debug(message)
     message.int_value))
   print(("  message.boolean_value: %s)"):format(
     message.boolean_value and "true" or "false"))
-  print("---------------------------------------------------------------------")
 
+end
+
+
+--[[ OneShotIdle Class ]]--
+
+-- delay a function call by the given amount of time into a tools idle notifier
+--
+-- for example: ´OneShotIdleNotifier(100, my_callback, some_arg, another_arg)´
+-- calls "my_callback" with the given arguments with a delay of about 100 ms
+-- a delay of 0 will call the callback "as soon as possible" in idle, but never
+-- immediately
+
+class "OneShotIdleNotifier"
+
+function OneShotIdleNotifier:__init(delay_in_ms, callback, ...)
+  assert(type(delay_in_ms) == "number" and delay_in_ms >= 0.0)
+  assert(type(callback) == "function")
+
+  self._callback = callback
+  self._args = arg
+  self._invoke_time = os.clock() + delay_in_ms / 1000
+
+  renoise.tool().app_idle_observable:add_notifier(self, self.__on_idle)
+end
+
+function OneShotIdleNotifier:__on_idle()
+  if (os.clock() >= self._invoke_time) then
+    renoise.tool().app_idle_observable:remove_notifier(self, self.__on_idle)
+    self._callback(unpack(self._args))
+  end
 end
