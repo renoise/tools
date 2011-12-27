c775e15c75cc93f5bf34f37301a510ce23f628b9
bjorn.nesby <bjorn.nesby@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2011-12-27 17:21:18 +0000

    Duplex * Fixed: missing XYPad config for Nocturn

---
+++ trunk/Tools/com.renoise.Duplex.xrnx/Duplex/Controllers/Nocturn/Configurations/Nocturn_XYPad.lua
@@ -0,0 +1,40 @@
+--[[----------------------------------------------------------------------------
+-- Duplex.Nocturn
+----------------------------------------------------------------------------]]--
+
+-- setup "XYPad" as the only app for this configuration
+
+duplex_configurations:insert {
+
+  -- configuration properties
+  name = "XYPad",
+  pinned = true,
+
+  -- device properties
+  device = {
+    class_name = nil,          
+    display_name = "Nocturn Automap",
+    device_port_in = "Automap MIDI",
+    device_port_out = "Automap MIDI",
+    control_map = "Controllers/Nocturn/Controlmaps/Nocturn.xml",
+    thumbnail = "Controllers/Nocturn/Nocturn.bmp",
+    protocol = DEVICE_MIDI_PROTOCOL
+  },
+  
+  applications = {
+    XYPad = {
+      mappings = {
+        x_slider = {
+          group_name = "Encoders",
+          index = 1,
+        },
+        y_slider = {
+          group_name = "Encoders",
+          index = 2,
+        },
+      },
+      options = {
+      }
+    }
+  }
+}
