8562bac3b0930abb1426a1b7d001ef28b6725300
illformed <illformed@82ecced6-6b67-a2d7-e476-c1ee4d4e15d4>
2012-01-20 16:53:22 +0000

    Lua snippet updates.

--- trunk/Snippets/PatternIterator.lua
+++ trunk/Snippets/PatternIterator.lua
@@ -15,7 +15,8 @@ local pattern_index =  renoise.song().selected_pattern_index
 
 for pos,line in pattern_iter:lines_in_pattern(pattern_index) do
   for _,note_column in pairs(line.note_columns) do 
-    if (note_column.is_selected and note_column.note_string == "C-4") then
+    if (note_column.is_selected and 
+        note_column.note_string == "C-4") then
       note_column.note_string = "E-4"
     end
   end
@@ -50,7 +51,7 @@ local arp_sequence = {
 for pos,line in pattern_iter:lines_in_pattern_track(pattern_index, track_index) do
   if not table.is_empty(line.note_columns) then
 
-    local note_column = line.note_columns[1]
+    local note_column = line:note_column(1)
     note_column:clear()
     
     local arp_index = math.mod(pos.line - 1, #arp_sequence) + 1
