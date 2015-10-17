0c20446b002bcfbcc44c7f8c2a65a15e6e026be0
bjorn-nesby <bjorn.nesby@gmail.com>
2015-10-17 21:47:01 +0200

    More details in description

--- Tools/com.renoise.xStream.xrnx/models/Args-Controls.lua
+++ Tools/com.renoise.xStream.xrnx/models/Args-Controls.lua
@@ -7,14 +7,14 @@ return {
 arguments = {
   {
       name = "value",
-      value = 3.154,
+      value = 99999,
       properties = {
           display_as = "value",
       },
   },
   {
       name = "hex",
-      value = 48,
+      value = 243,
       properties = {
           min = 0,
           display_as = "hex",
@@ -23,14 +23,14 @@ arguments = {
   },
   {
       name = "integer",
-      value = 34,
+      value = 11,
       properties = {
           display_as = "integer",
       },
   },
   {
       name = "percent",
-      value = 67.441860465116,
+      value = 7.2,
       properties = {
           min = -100,
           display_as = "percent",
@@ -39,7 +39,7 @@ arguments = {
   },
   {
       name = "note",
-      value = 70.74688372093,
+      value = 48,
       properties = {
           min = 0,
           display_as = "note",
@@ -60,7 +60,7 @@ arguments = {
   },
   {
       name = "chooser",
-      value = 2,
+      value = 1,
       properties = {
           display_as = "chooser",
           items = {
@@ -72,7 +72,7 @@ arguments = {
   },
   {
       name = "switch",
-      value = 2,
+      value = 1,
       properties = {
           display_as = "switch",
           items = {
@@ -84,28 +84,28 @@ arguments = {
   },
   {
       name = "minislider",
-      value = -44222.813581395,
+      value = 35999.64,
       properties = {
           display_as = "minislider",
       },
   },
   {
       name = "rotary",
-      value = -39999.6,
+      value = 55999.44,
       properties = {
           display_as = "rotary",
       },
   },
   {
       name = "boolean",
-      value = true,
+      value = false,
       properties = {
           display_as = "boolean",
       },
   },
   {
       name = "string",
-      value = "this is another string",
+      value = "this is a string",
       properties = {
           display_as = "string",
       },
@@ -117,28 +117,28 @@ presets = {
       note = 48,
       integer = 11,
       string = "this is a string",
-      boolean = true,
+      boolean = false,
       popup = 2,
       chooser = 1,
       minislider = 35999.64,
-      value = 4,
+      switch = 1,
       rotary = -39999.6,
       percent = 7.2,
-      switch = 1,
+      value = 4,
   },
   {
       hex = 48,
       note = 70.74688372093,
       integer = 34,
       string = "this is another string",
-      boolean = true,
+      boolean = false,
       popup = 2,
       chooser = 2,
       minislider = -44222.813581395,
-      value = 3.154,
+      switch = 2,
       rotary = -39999.6,
       percent = 67.441860465116,
-      switch = 2,
+      value = 3.154,
   },
 },
 data = {
@@ -154,20 +154,21 @@ callback = [[
 -- at the same time, or click [models > magnifying glass] to reveal the 
 -- location of the .lua file in which the actual arguments are defined)
 -------------------------------------------------------------------------------
--- Name       Type      Supports      Requires  Restrict
---                      'zero_based'  property  to range
--- VALUE      number
--- HEX        number    yes
--- INTEGER    number    yes
--- PERCENT    number
--- NOTE       number                            1-119
--- POPUP      number                  items
--- CHOOSER    number                  items
--- SWITCH     number                  items
--- MINISLIDER number
--- ROTARY     number
--- BOOLEAN    boolean
--- STRING     string
+-- Name       Type      Supports      Supports  Requires  Restrict  
+--                      'zero_based'  min/max   property  to range
+-- VALUE      number                  yes                 
+-- HEX        number    yes           yes                 
+-- INTEGER    number    yes           yes                 
+-- PERCENT    number                  yes                 
+-- NOTE       number                  yes                 1-119
+-- POPUP      number                            items     
+-- CHOOSER    number                            items     
+-- SWITCH     number                            items     
+-- MINISLIDER number                  yes                 
+-- ROTARY     number                  yes                 
+-- BOOLEAN    boolean                                     
+-- STRING     string                                      
+
 
 
 
