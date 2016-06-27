e00506466b45e2ee8b77d71ba2553b819924fedc
bjorn-nesby <bjorn.nesby@gmail.com>
2016-06-27 11:50:56 +0200

    API4 -> API5

--- Libraries/renoise/http/request.lua
+++ Libraries/renoise/http/request.lua
@@ -1225,7 +1225,7 @@ function Request:_do_callback(socket_error)
           local cd = self:_get_header("Content-Disposition")          
           if (cd and #cd > 0) then
             log:info("Content-Disposition: " .. cd)
-            filename = cd:match('filename\s*=\s*(.+)$')
+            filename = cd:match('filename%s*=%s*(.+)$')
             -- remove surrounding quotes, when present
             if (filename and filename:match('^"(.+)"$')) then
               filename = filename:match('^"(.+)"$')
