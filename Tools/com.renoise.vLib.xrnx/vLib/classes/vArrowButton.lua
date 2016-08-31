b7198089d2ee088ba46a7b1dc40e3012068eb5f7
bjorn-nesby <bjorn.nesby@gmail.com>
2016-08-31 14:04:31 +0200

    New widgets: vArrowButton, vToggleButton, vButton

---
+++ Tools/com.renoise.vLib.xrnx/vLib/classes/vArrowButton.lua
@@ -0,0 +1,104 @@
+--[[============================================================================
+vArrowButton 
+============================================================================]]--
+--[[
+
+A button which emulates the 'toggle arrow' button in Renoise
+
+]]
+
+--==============================================================================
+
+require (_vlibroot.."vToggleButton")
+
+class 'vArrowButton' (vToggleButton)
+
+vArrowButton.ORIENTATION = {
+  HORIZONTAL = 1,
+  VERTICAL = 2,
+}
+
+vArrowButton.BUTTONS = {
+  HORIZONTAL = {"◂","▸"},
+  VERTICAL = {"▾","▴"},
+}
+
+--------------------------------------------------------------------------------
+
+function vArrowButton:__init(...)
+  TRACE("vArrowButton:__init()")
+
+  local args = cLib.unpack_args(...)
+
+  -- properties -----------------------
+
+  self.orientation = property(self.get_orientation,self.set_orientation)
+  self.orientation_observable = renoise.Document.ObservableNumber(args.orientation or vArrowButton.ORIENTATION.VERTICAL)
+
+  self.flipped = property(self.get_flipped,self.set_flipped)
+  self.flipped_observable = renoise.Document.ObservableBoolean(args.flipped or true)
+
+  -- override default value
+  args.width = args.width or 18
+  args.width = args.height or 18
+  args.color_enabled = vLib.COLOR_SELECTED
+  args.color_disabled = vLib.COLOR_NORMAL
+
+  -- internal -------------------------
+
+	vToggleButton.__init(self,...)
+  self:update_text()
+
+  print(">>> self:update()")
+  self:update()
+
+end
+
+--------------------------------------------------------------------------------
+--[[
+function vArrowButton:update()
+  print("vArrowButton:update()")
+
+  vToggleButton.update(self)
+
+end
+]]
+--------------------------------------------------------------------------------
+
+function vArrowButton:update_text()
+
+  local buttons = (self.orientation == vArrowButton.ORIENTATION.HORIZONTAL) 
+    and vArrowButton.BUTTONS.HORIZONTAL or vArrowButton.BUTTONS.VERTICAL
+  print("*** update - buttons",rprint(buttons))
+  print("*** update - flipped",self.flipped)
+
+  self.text_enabled = self.flipped and buttons[2] or buttons[1]
+  self.text_disabled = self.flipped and buttons[1] or buttons[2]
+
+  self:request_update()
+
+end
+
+--------------------------------------------------------------------------------
+
+function vArrowButton:set_orientation(val)
+  self.orientation_observable.value = val
+  self:update_text()
+end
+
+function vArrowButton:get_orientation()
+  return self.orientation_observable.value
+end
+
+--------------------------------------------------------------------------------
+
+function vArrowButton:set_flipped(val)
+  self.flipped_observable.value = val
+  self:update_text()
+end
+
+function vArrowButton:get_flipped()
+  return self.flipped_observable.value
+end
+
+
---
+++ Tools/com.renoise.vLib.xrnx/vLib/classes/vButton.lua
@@ -0,0 +1,184 @@
+--[[============================================================================
+vButton 
+============================================================================]]--
+--[[
+
+A basic button
+
+]]
+
+--==============================================================================
+
+require (_vlibroot.."vControl")
+
+class 'vButton' (vControl)
+
+--------------------------------------------------------------------------------
+
+function vButton:__init(...)
+  print("vButton:__init()")
+
+  local args = cLib.unpack_args(...)
+
+  -- properties -----------------------
+
+  self.text = property(self.get_text,self.set_text)
+  self._text = args.text or ""
+
+  self.bitmap = property(self.get_bitmap,self.set_bitmap)
+  self._bitmap = args.bitmap or ""
+
+  self.color = property(self.get_color,self.set_color)
+  self._color = args.color or {0x00,0x00,0x00}
+
+  -- specify defaults
+  args.width = args.width or 50
+  args.height = args.height or 21
+
+  -- methods --------------------------
+
+  self.pressed = args.pressed or nil
+  self.released = args.released or nil
+  self.notifier = args.notifier or nil
+
+  -- initialize -----------------------
+
+  vControl.__init(self,...)
+
+  self:build()
+
+  -- TODO investigate returning view in constructor
+  -- (declare within viewbuilder structures)
+  --return self.view
+
+end
+
+--------------------------------------------------------------------------------
+
+function vButton:build()
+  print(">>> vButton:build()")
+	local vb = self.vb
+  if not self.view then
+    self.view = vb:button{
+      id = self.id,
+      pressed = function()
+        self:press()
+      end,
+      released = function()
+        self:release()
+      end,
+    }
+  end
+  self:request_update()
+end
+
+--------------------------------------------------------------------------------
+
+function vButton:press()
+  if self.pressed then
+    self:pressed()
+  end
+end
+
+--------------------------------------------------------------------------------
+
+function vButton:release()
+  print(">>> vButton:release()")
+  if self.released then
+    self:released()
+  end
+  if self.notifier then
+    self:notifier()
+  end
+end
+
+--------------------------------------------------------------------------------
+
+function vButton:update()
+  print("vButton:update()")
+
+  print(">>> self.view.bitmap",self.view.bitmap)
+  print(">>> self.view.text",self.view.text)
+  print(">>> self.view.color",self.view.color)
+  print(">>> self.view.midi_mapping",self.view.midi_mapping)
+  print(">>> self.view.width",self.view.width)
+  print(">>> self.view.height",self.view.height)
+
+  if self.text then self.view.text = self.text end
+  if self.color then self.view.color = self.color end
+  if self.bitmap then self.view.bitmap = self.bitmap end
+  if self.midi_mapping then self.view.midi_mapping = self.midi_mapping end
+  if self._width then self.view.width = self._width end
+  if self._height then self.view.height = self._height end
+
+
+end
+
+--------------------------------------------------------------------------------
+
+function vButton:set_active(b)
+  self.view.active = b
+end
+
+--------------------------------------------------------------------------------
+
+function vButton:set_text(val)
+  assert(type(val)=="string")
+  self._text = val
+  self.view.text = val
+end
+
+function vButton:get_text()
+  return self._text
+end
+
+--------------------------------------------------------------------------------
+
+function vButton:set_bitmap(val)
+  assert(type(val)=="string")
+  self._bitmap = val
+  self.view.bitmap = val
+end
+
+function vButton:get_bitmap()
+  return self._bitmap
+end
+
+--------------------------------------------------------------------------------
+
+function vButton:set_color(val)
+  assert(type(val)=="table")
+  self._color = val
+  self.view.color = val
+end
+
+function vButton:get_color()
+  return self._color
+end
+
+--------------------------------------------------------------------------------
+
+function vButton:set_midi_mapping(val)
+  assert(type(val)=="string")
+  vControl.set_midi_mapping(self,val)
+  self.view.midi_mapping = val
+end
+
+--------------------------------------------------------------------------------
+
+function vButton:set_width(val)
+  print(">>> vButton:set_width(val)")
+  assert(type(val)=="number")
+  self.view.width = val
+  vControl.set_width(self)
+end
+
+--------------------------------------------------------------------------------
+
+function vButton:set_height(val)
+  print(">>> vButton:set_height(val)")
+  assert(type(val)=="number")
+  self.view.height = val
+  vControl.set_height(self)
+end
+
