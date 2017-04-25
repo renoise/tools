9d049b6b4edaf4916a4d0b6ed18884a9844b2583
bjorn-nesby <bjorn.nesby@gmail.com>
2017-04-25 13:17:29 +0200

    - get rid of obsolete properties

--- Tools/com.renoise.SliceMate.xrnx/source/SliceMate_Prefs.lua
+++ Tools/com.renoise.SliceMate.xrnx/source/SliceMate_Prefs.lua
@@ -15,21 +15,17 @@ class 'SliceMate_Prefs'(renoise.Document.DocumentNode)
 function SliceMate_Prefs:__init()
 
   renoise.Document.DocumentNode.__init(self)
+  -- tool options
   self:add_property("autostart", renoise.Document.ObservableBoolean(true))
   self:add_property("show_on_launch", renoise.Document.ObservableBoolean(true))
-
+  -- slice settings
   self:add_property("autoselect_instr", renoise.Document.ObservableBoolean(true))
   self:add_property("autoselect_in_wave", renoise.Document.ObservableBoolean(true))
   self:add_property("autoselect_in_list", renoise.Document.ObservableBoolean(true))
   self:add_property("quantize_enabled", renoise.Document.ObservableBoolean(false))
   self:add_property("insert_note", renoise.Document.ObservableBoolean(true))
   self:add_property("propagate_vol_pan", renoise.Document.ObservableBoolean(true))
-  self:add_property("note", renoise.Document.ObservableBoolean(true))
-  self:add_property("instrument", renoise.Document.ObservableBoolean(true))
-  self:add_property("volume", renoise.Document.ObservableBoolean(false))
-  self:add_property("panning", renoise.Document.ObservableBoolean(false))
-  self:add_property("delay", renoise.Document.ObservableBoolean(false))
-  self:add_property("match_all_criteria", renoise.Document.ObservableBoolean(true))
+  -- remember UI state
   self:add_property("show_tool_options", renoise.Document.ObservableBoolean(false))
   self:add_property("show_slice_options", renoise.Document.ObservableBoolean(false))
   
