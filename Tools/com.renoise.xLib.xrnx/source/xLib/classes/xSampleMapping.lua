a5d9fc88b2f27731cea0e2dbd56b36f961a8f1da
bjorn-nesby <bjorn.nesby@gmail.com>
2017-04-27 15:37:55 +0200

    - fix typo

--- Tools/com.renoise.xLib.xrnx/source/xLib/classes/xSampleMapping.lua
+++ Tools/com.renoise.xLib.xrnx/source/xLib/classes/xSampleMapping.lua
@@ -16,13 +16,21 @@ Static methods for working with renoise sample mappings
 class 'xSampleMapping'
 
 --------------------------------------------------------------------------------
+
+function xSampleMapping.within_note_range(note,mapping)
+  TRACE("xSampleMapping.within_note_range(note,mapping)",note,mapping)
+  local rng = mapping.note_range
+  return (note >= rng[1]) and (note <= rng[2]) 
+end
+
+--------------------------------------------------------------------------------
 --- Shift samples by amount of semitones, starting from the sample index 
 -- @param instr (renoise.Instrument)
 -- @param sample_idx_from (int)
 -- @param amt (int)
 
 function xSampleMapping.shift_keyzone_by_semitones(instr,sample_idx_from,amt)
-  TRACE("xLib.shift_keyzone_by_semitones(instr,sample_idx_from,amt)",instr,sample_idx_from,amt)
+  TRACE("xSampleMapping.shift_keyzone_by_semitones(instr,sample_idx_from,amt)",instr,sample_idx_from,amt)
 
   for sample_idx = sample_idx_from,#instr.samples do
     local sample = instr.samples[sample_idx]
