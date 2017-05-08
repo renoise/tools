adbbb9a65043929eacf2aecd0beca8ad3e3ec793
bjorn-nesby <bjorn.nesby@gmail.com>
2017-05-08 13:14:56 +0200

    - cLib integration
    - update descriptions

--- Tools/com.renoise.Duplex.xrnx/Duplex/Applications/Transport.lua
+++ Tools/com.renoise.Duplex.xrnx/Duplex/Applications/Transport.lua
@@ -87,51 +87,43 @@ Transport.default_options = {
 --
 Transport.available_mappings = {
   stop_playback = {
-    description = "Transport: Stop playback"
-                .."\nControl value: ",
+    description = "Transport: Stop playback",
   },
   start_playback = {
-    description = "Transport: Start playback"    
-                .."\nControl value: ",
+    description = "Transport: Start playback",
   },
   loop_pattern = {
-    description = "Transport: Toggle pattern looping"
-                .."\nControl value: ",
+    description = "Transport: Toggle pattern looping",
   },
   edit_mode = {
-    description = "Transport: Toggle edit-mode"
-                 .."\nControl value: ",
+    description = "Transport: Toggle edit-mode",
   },
   follow_player = {
-    description = "Transport: Toggle play-follow mode"
-                .."\nControl value: ",
+    description = "Transport: Toggle play-follow mode",
   },
   goto_next = {
-    description = "Transport: Goto next pattern/block"    
-                .."\nControl value: ",
+    description = "Transport: Goto next pattern/block",    
   },
   goto_previous = {
-    description = "Transport: Goto previous pattern/block"    
-                .."\nControl value: ",
+    description = "Transport: Goto previous pattern/block",    
   },
   block_loop = {
-    description = "Transport: Toggle block-loop mode"    
-                .."\nControl value: ",
+    description = "Transport: Toggle block-loop mode",    
   },
   metronome_toggle = {
-    description = "Transport: toggle on/off",
+    description = "Transport: Toggle Metronome on/off",
   },
   bpm_increase = {
-    description = "Transport: increase BPM",
+    description = "Transport: Increase BPM",
   },
   bpm_decrease = {
-    description = "Transport: decrease BPM",
+    description = "Transport: Decrease BPM",
   },
   bpm_display = {
-    description = "Transport: display current BPM",
+    description = "Transport: Display current BPM",
   },
   songpos_display = {
-    description = "Transport: display song-position",
+    description = "Transport: Display song-position",
   }
 }
 
@@ -192,6 +184,7 @@ function Transport:__init(...)
 
   Application.__init(self,...)
 
+  --self:list_mappings_and_options(Transport.available_mappings,Transport.default_options)
 
 end
 
@@ -223,8 +216,8 @@ function Transport:on_idle()
     return 
   end
 
-  local loop_block_enabled = renoise.song().transport.loop_block_enabled
-  local loop_pattern = renoise.song().transport.loop_pattern
+  local loop_block_enabled = rns.transport.loop_block_enabled
+  local loop_pattern = rns.transport.loop_pattern
 
   -- update 'block_loop' status
   if (self._block_loop ~= loop_block_enabled) then
@@ -234,7 +227,7 @@ function Transport:on_idle()
 
   -- check if we have arrived at the scheduled pattern
   if (self._scheduled_pattern)then
-    local pos = renoise.song().transport.playback_pos.sequence
+    local pos = rns.transport.playback_pos.sequence
     if(self._scheduled_pattern == pos) or
       (pos~=self._source_pattern) then
       
@@ -247,12 +240,12 @@ function Transport:on_idle()
 
   -- update song-position display
   if self.controls.songpos_display then
-    local pos_beats = renoise.song().transport.playback_pos_beats
+    local pos_beats = rns.transport.playback_pos_beats
     if pos_beats ~= self._pos_beats then
       --print("*** pos_beats",pos_beats)
       self._pos_beats = pos_beats
       -- format output like this: bars:beats.fraction (16:03.2)
-      local lpb = renoise.song().transport.lpb
+      local lpb = rns.transport.lpb
       local bars = math.floor(pos_beats/lpb)
       local beats = (math.floor(pos_beats)%lpb)
       local fraction = select(2,math.modf(pos_beats))*100
@@ -270,11 +263,11 @@ end
 
 function Transport:update_scheduled_buttons()
 
-    local playing = renoise.song().transport.playing
-    local lpb = renoise.song().transport.lpb
+    local playing = rns.transport.playing
+    local lpb = rns.transport.lpb
     local pos = playing 
-      and renoise.song().transport.playback_pos 
-      or renoise.song().transport.edit_pos
+      and rns.transport.playback_pos 
+      or rns.transport.edit_pos
     -- 'blink' when we have moved a certain distance
     local is_prev = (self._scheduled_button == self.controls.previous)
     local blink = (math.floor((((pos.line-1)/lpb))%2) == 0)
@@ -323,6 +316,7 @@ end
 function Transport:on_new_document()
   TRACE("Transport.on_new_document()")
 
+  rns = renoise.song()
   self:_attach_to_song()
 
 end
@@ -334,10 +328,8 @@ end
 function Transport:_attach_to_song()
   TRACE("Transport._attach_to_song()")
 
-  local song = renoise.song()
-
   -- metronome --
-  song.transport.metronome_enabled_observable:add_notifier(
+  rns.transport.metronome_enabled_observable:add_notifier(
     function()
       TRACE("Transport.metronome_enabled_observable fired...")
       self:update_metronome_enabled()
@@ -345,7 +337,7 @@ function Transport:_attach_to_song()
   )
 
   -- follow player --
-  song.transport.follow_player_observable:add_notifier(
+  rns.transport.follow_player_observable:add_notifier(
     function()
       TRACE("Transport.follow_player_observable fired...")
       self:update_follow_player()
@@ -353,11 +345,11 @@ function Transport:_attach_to_song()
   )
 
   -- playing --
-  song.transport.playing_observable:add_notifier(
+  rns.transport.playing_observable:add_notifier(
     function()
       TRACE("Transport.playing_observable fired...")
       self:update_playing()
-      if not renoise.song().transport.playing then
+      if not rns.transport.playing then
         self:clear_schedule()
       end
 
@@ -365,7 +357,7 @@ function Transport:_attach_to_song()
   )
 
   -- loop pattern -- 
-  song.transport.loop_pattern_observable:add_notifier(
+  rns.transport.loop_pattern_observable:add_notifier(
     function()
       TRACE("Transport.loop_pattern_observable fired...")
       self:update_loop_pattern()
@@ -373,14 +365,14 @@ function Transport:_attach_to_song()
   )
 
   -- edit mode --
-  song.transport.edit_mode_observable:add_notifier(
+  rns.transport.edit_mode_observable:add_notifier(
     function()
       TRACE("Transport.edit_mode_observable fired...")
       self:update_edit_mode()
     end
   )
 
-  song.transport.bpm_observable:add_notifier(
+  rns.transport.bpm_observable:add_notifier(
     function()
       TRACE("Transport.bpm_observable fired...")
       self:update_bpm_display()
@@ -399,7 +391,7 @@ function Transport:update_bpm_display()
 
   --print("*** self.controls.bpm_display",self.controls.bpm_display)
   if self.controls.bpm_display then
-    self.controls.bpm_display:set_text(renoise.song().transport.bpm)
+    self.controls.bpm_display:set_text(rns.transport.bpm)
   end
 
 end
@@ -414,7 +406,7 @@ function Transport:update_metronome_enabled()
     return false 
   end
   if self.controls.metronome_toggle then
-    if (renoise.song().transport.metronome_enabled) then
+    if (rns.transport.metronome_enabled) then
       self.controls.metronome_toggle:set(self.palette.metronome_on)
     else
       self.controls.metronome_toggle:set(self.palette.metronome_off)
@@ -431,7 +423,7 @@ function Transport:update_follow_player()
     return false 
   end
   if self.controls.follow_player then
-    if (renoise.song().transport.follow_player) then
+    if (rns.transport.follow_player) then
       self.controls.follow_player:set(self.palette.follow_player_on)
     else
       self.controls.follow_player:set(self.palette.follow_player_off)
@@ -448,7 +440,7 @@ function Transport:update_playing()
     return false 
   end
   if self.controls.play then
-    if renoise.song().transport.playing then
+    if rns.transport.playing then
       self.controls.play:set(self.palette.playing_on)
     else
       self.controls.play:set(self.palette.playing_off)
@@ -465,7 +457,7 @@ function Transport:update_loop_pattern()
     return false 
   end
   if self.controls.loop then
-    if renoise.song().transport.loop_pattern then
+    if rns.transport.loop_pattern then
       self.controls.loop:set(self.palette.loop_pattern_on)
     else
       self.controls.loop:set(self.palette.loop_pattern_off)
@@ -482,7 +474,7 @@ function Transport:update_edit_mode()
     return false 
   end
   if self.controls.edit then
-    if renoise.song().transport.edit_mode then
+    if rns.transport.edit_mode then
       self.controls.edit:set(self.palette.edit_mode_on)
     else
       self.controls.edit:set(self.palette.edit_mode_off)
@@ -499,7 +491,7 @@ function Transport:update_block_loop()
     return false 
   end
   if self.controls.block then
-    if renoise.song().transport.loop_block_enabled then
+    if rns.transport.loop_block_enabled then
       self.controls.block:set(self.palette.loop_block_on)
     else
       self.controls.block:set(self.palette.loop_block_off)
@@ -567,8 +559,8 @@ function Transport:_build_app()
     c.tooltip = map.description
     c:set_pos(map.index)
     c.on_press = function(obj)
-      local loop_pattern = renoise.song().transport.loop_pattern
-      renoise.song().transport.loop_pattern = not loop_pattern
+      local loop_pattern = rns.transport.loop_pattern
+      rns.transport.loop_pattern = not loop_pattern
     end
     self.controls.loop = c
   end
@@ -580,8 +572,8 @@ function Transport:_build_app()
     c.tooltip = map.description
     c:set_pos(map.index)
     c.on_press = function(obj)
-      local edit_mode = renoise.song().transport.edit_mode
-      renoise.song().transport.edit_mode = not edit_mode
+      local edit_mode = rns.transport.edit_mode
+      rns.transport.edit_mode = not edit_mode
     end
     self.controls.edit = c
   end
@@ -619,8 +611,8 @@ function Transport:_build_app()
     c.tooltip = map.description
     c:set_pos(map.index)
     c.on_press = function(obj)
-      local block_enabled = renoise.song().transport.loop_block_enabled
-      renoise.song().transport.loop_block_enabled = not block_enabled
+      local block_enabled = rns.transport.loop_block_enabled
+      rns.transport.loop_block_enabled = not block_enabled
     end
     self.controls.block = c
   end
@@ -632,8 +624,8 @@ function Transport:_build_app()
     c.tooltip = map.description
     c:set_pos(map.index)
     c.on_press = function(obj)
-      local active = renoise.song().transport.follow_player
-      renoise.song().transport.follow_player = not active
+      local active = rns.transport.follow_player
+      rns.transport.follow_player = not active
     end
     self.controls.follow_player = c
   end
@@ -645,8 +637,8 @@ function Transport:_build_app()
     c.tooltip = map.description
     c:set_pos(map.index)
     c.on_press = function(obj)
-      local metronome_enabled = renoise.song().transport.metronome_enabled
-      renoise.song().transport.metronome_enabled = not metronome_enabled
+      local metronome_enabled = rns.transport.metronome_enabled
+      rns.transport.metronome_enabled = not metronome_enabled
     end
     self.controls.metronome_toggle = c
   end
@@ -659,9 +651,9 @@ function Transport:_build_app()
     c:set_pos(map.index)
     c:set(self.palette.bpm_increase_off)
     c.on_press = function(obj)
-      local bpm = math.min(BPM_MAXIMUM,renoise.song().transport.bpm+1)
-      if (bpm~=renoise.song().transport.bpm) then
-        renoise.song().transport.bpm = bpm
+      local bpm = math.min(BPM_MAXIMUM,rns.transport.bpm+1)
+      if (bpm~=rns.transport.bpm) then
+        rns.transport.bpm = bpm
         self.controls.bpm_increase:flash(0.1,
           self.palette.bpm_increase_on,
           self.palette.bpm_increase_off)
@@ -678,9 +670,9 @@ function Transport:_build_app()
     c:set_pos(map.index)
     c:set(self.palette.bpm_decrease_off)
     c.on_press = function(obj)
-      local bpm = math.max(BPM_MINIMUM,renoise.song().transport.bpm-1)
-      if (bpm~=renoise.song().transport.bpm) then
-        renoise.song().transport.bpm = bpm
+      local bpm = math.max(BPM_MINIMUM,rns.transport.bpm-1)
+      if (bpm~=rns.transport.bpm) then
+        rns.transport.bpm = bpm
         self.controls.bpm_decrease:flash(0.1,
           self.palette.bpm_decrease_on,
           self.palette.bpm_decrease_off)
@@ -719,7 +711,7 @@ end
 function Transport:_start_playback()
   TRACE("Transport:_start_playback()")
 
-  if renoise.song().transport.playing then
+  if rns.transport.playing then
     -- retriggered
     if (self.options.pattern_play.value == PLAY_MODE_RETRIG) then
       self:_retrigger_pattern()
@@ -730,8 +722,8 @@ function Transport:_start_playback()
     end
   else
     -- we started playing
-    local pos = renoise.song().transport.playback_pos.sequence
-    renoise.song().transport:trigger_sequence(pos)
+    local pos = rns.transport.playback_pos.sequence
+    rns.transport:trigger_sequence(pos)
 
   end
 
@@ -744,9 +736,9 @@ end
 function Transport:_stop_playback()
   TRACE("Transport:_stop_playback()")
 
-  if (not renoise.song().transport.playing) then
+  if (not rns.transport.playing) then
     if (self.options.pattern_stop.value == STOP_MODE_PANIC) then
-      renoise.song().transport:panic()
+      rns.transport:panic()
     elseif (self.options.pattern_stop.value == STOP_MODE_JUMP) then
       self:_jump_to_beginning()
     end
@@ -754,7 +746,7 @@ function Transport:_stop_playback()
 
   self:clear_schedule()
 
-  renoise.song().transport.playing = false
+  rns.transport.playing = false
 
   if(self.controls.play)then
     self.controls.play:set(self.palette.playing_off)
@@ -772,11 +764,11 @@ function Transport:_next()
   local block_mode = (self.options.jump_mode.value == JUMP_MODE_BLOCK)
   if self._block_loop and block_mode then
     -- move to next block loop
-    renoise.song().transport:loop_block_move_forwards()
+    rns.transport:loop_block_move_forwards()
   else
     if (self.options.pattern_switch.value == SWITCH_MODE_SWITCH) then
       -- switch instantly
-      local new_pos = renoise.song().transport.playback_pos
+      local new_pos = rns.transport.playback_pos
       self:_switch_to_seq_index(new_pos.sequence+1)
       if self.controls.next then
         self.controls.next:flash(0.1,
@@ -787,8 +779,8 @@ function Transport:_next()
     else
       -- schedule
       local pos = self._scheduled_pattern or 
-        renoise.song().transport.playback_pos.sequence
-      local seq_count = #renoise.song().sequencer.pattern_sequence
+        rns.transport.playback_pos.sequence
+      local seq_count = #rns.sequencer.pattern_sequence
       if(pos < seq_count)then
         pos = pos+1
       end
@@ -808,11 +800,11 @@ function Transport:_previous()
   local block_mode = (self.options.jump_mode.value == JUMP_MODE_BLOCK)
   if self._block_loop and block_mode then
     -- move to previous block loop
-    renoise.song().transport:loop_block_move_backwards()
+    rns.transport:loop_block_move_backwards()
   else
     if (self.options.pattern_switch.value == SWITCH_MODE_SWITCH) then
       -- switch instantly
-      local new_pos = renoise.song().transport.playback_pos
+      local new_pos = rns.transport.playback_pos
       self:_switch_to_seq_index(new_pos.sequence-1)
       if self.controls.previous then
         self.controls.previous:flash(0.1,
@@ -823,7 +815,7 @@ function Transport:_previous()
     else
       -- schedule
       local pos = self._scheduled_pattern or 
-        renoise.song().transport.playback_pos.sequence
+        rns.transport.playback_pos.sequence
       if(pos > 1)then
         pos = pos-1
       end
@@ -844,8 +836,8 @@ function Transport:_schedule_pattern(idx,button)
 
   self._scheduled_pattern = idx
   self._scheduled_button = button
-  self._source_pattern = renoise.song().transport.playback_pos.sequence
-  renoise.song().transport:set_scheduled_sequence(idx)
+  self._source_pattern = rns.transport.playback_pos.sequence
+  rns.transport:set_scheduled_sequence(idx)
 
 end
 
@@ -856,8 +848,8 @@ end
 function Transport:_retrigger_pattern()
   TRACE("Transport:_retrigger_pattern()")
 
-  local pos = renoise.song().transport.playback_pos
-  renoise.song().transport:trigger_sequence(pos.sequence)
+  local pos = rns.transport.playback_pos
+  rns.transport:trigger_sequence(pos.sequence)
 
 end
 
@@ -869,8 +861,8 @@ function Transport:_reschedule_pattern()
   TRACE("Transport:_reschedule_pattern()")
 
   self:clear_schedule()
-  local pos = renoise.song().transport.playback_pos.sequence
-  renoise.song().transport:set_scheduled_sequence(pos)
+  local pos = rns.transport.playback_pos.sequence
+  rns.transport:set_scheduled_sequence(pos)
 
 end
 
@@ -882,17 +874,16 @@ end
 function Transport:_switch_to_seq_index(seq_index)
   TRACE("Transport:_switch_to_seq_index()",seq_index)
 
-  local song = renoise.song()
-  local new_pos = song.transport.playback_pos
-  local patt_idx = song.sequencer.pattern_sequence[seq_index]
-  local patt = song.patterns[patt_idx]
+  local new_pos = rns.transport.playback_pos
+  local patt_idx = rns.sequencer.pattern_sequence[seq_index]
+  local patt = rns.patterns[patt_idx]
   if patt then
-    local num_lines = song.patterns[patt_idx].number_of_lines
+    local num_lines = rns.patterns[patt_idx].number_of_lines
     if(new_pos.line>num_lines)then
       new_pos.line = 1
     end
     new_pos.sequence = seq_index
-    song.transport.playback_pos = new_pos
+    rns.transport.playback_pos = new_pos
   end
 
 end
@@ -904,10 +895,10 @@ end
 function Transport:_jump_to_beginning()
   TRACE("Transport:_jump_to_beginning()")
 
-  local new_pos = renoise.song().transport.playback_pos
+  local new_pos = rns.transport.playback_pos
   new_pos.sequence = 1
   new_pos.line = 1
-  renoise.song().transport.playback_pos = new_pos
+  rns.transport.playback_pos = new_pos
 
 end
 
