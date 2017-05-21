19e803f114c348cd1d398a2df9adcf8f6dd02697
bjorn-nesby <bjorn.nesby@gmail.com>
2017-05-21 15:33:06 +0200

    - update model

--- Tools/com.renoise.xStream.xrnx/userdata/models/Demo-Scheduling.lua
+++ Tools/com.renoise.xStream.xrnx/userdata/models/Demo-Scheduling.lua
@@ -1,5 +1,5 @@
 --[[===========================================================================
-Scheduling.lua
+Demo-Scheduling.lua
 ===========================================================================]]--
 
 return {
@@ -16,17 +16,41 @@ options = {
 },
 callback = [[
 -------------------------------------------------------------------------------
--- Create a burst of scheduled events
+-- Scheduling output: 
+-- Most models are generating/transforming pattern data on a line-by-line
+-- basis, but it's possible to schedule things to happen at a later time too,
+-- by using the schedule_line/note_column/effect_column() methods.
 -------------------------------------------------------------------------------
+
+-- all scheduling happens on the first line: 
 if (xinc == 1) then  
-  -- scheduling a whole line (will clear existing)
-  xbuffer:schedule_line({note_columns = {{note_string = "C-4"}}},3)  
-  -- scheduling note columns (will retain other content)
-  xbuffer:schedule_note_column({note_string="C-4"},2,5)
+
+  -- ==== schedule a whole line ====
+  -- (the last argument is the buffer position)
+
+  xbuffer:schedule_line({
+    note_columns = {
+      {note_string = "C-4"},
+      {note_string = "D-4"},      
+      {note_string = "E-4"},      
+     }
+    },3) -- buffer position
+
+  -- ==== schedule note columns ====
+  -- (the last two arguments are column index and buffer position)
+
+  xbuffer:schedule_note_column({note_string="C-4"},3,5)
   xbuffer:schedule_note_column({note_string="C-4"},1,8)
-  -- scheduling effect columns (will retain other content)
-  xbuffer:schedule_effect_column({value_string="11"},1,3)
-  xbuffer:schedule_effect_column({amount_string="22"},1,4)
+
+  -- ==== schedule effect columns ====
+  -- (the last two arguments are column index and buffer position)
+
+  xbuffer:schedule_effect_column({number_string="11"},1,3)
+  xbuffer:schedule_effect_column({
+    number_string = "0Z",
+    amount_string = "22"
+  },1,2)
+  
 end
 ]],
 }
