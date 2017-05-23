cf570226763b7140f96cea303e2788f3b2cd5239
bjorn-nesby <bjorn.nesby@gmail.com>
2017-05-23 13:00:20 +0200

    - improved scheduling example

--- Tools/com.renoise.xStream.xrnx/userdata/models/Demo-Scheduling.lua
+++ Tools/com.renoise.xStream.xrnx/userdata/models/Demo-Scheduling.lua
@@ -22,34 +22,35 @@ callback = [[
 -- by using the schedule_line/note_column/effect_column() methods.
 -------------------------------------------------------------------------------
 
--- all scheduling happens on the first line: 
-if (xinc == 1) then  
+-- scheduling happens on the very first line - this model doesn't 
+-- do anything beyond that point ... 
 
-  -- ==== schedule a whole line ====
-  -- (the last argument is the buffer position)
+if (xinc == 0) then  
 
+  -- schedule three note columns for line #00:
+  
   xbuffer:schedule_line({
     note_columns = {
       {note_string = "C-4"},
       {note_string = "D-4"},      
       {note_string = "E-4"},      
      }
-    },3) -- buffer position
+    },0) -- buffer position
 
-  -- ==== schedule note columns ====
-  -- (the last two arguments are column index and buffer position)
+  -- next, schedule note columns 2 & 3, output at line 1 & 2:
 
-  xbuffer:schedule_note_column({note_string="C-4"},3,5)
-  xbuffer:schedule_note_column({note_string="C-4"},1,8)
+  xbuffer:schedule_note_column({note_string="F-4"},2,1)
+  xbuffer:schedule_note_column({note_string="G-4"},3,2)
 
-  -- ==== schedule effect columns ====
-  -- (the last two arguments are column index and buffer position)
+  -- finally, schedule lines 3 & 4 for effect column #1
 
-  xbuffer:schedule_effect_column({number_string="11"},1,3)
+  xbuffer:schedule_effect_column({
+    number_string="11"
+  },1,3)
   xbuffer:schedule_effect_column({
     number_string = "0Z",
     amount_string = "22"
-  },1,2)
+  },1,4)
   
 end
 ]],
