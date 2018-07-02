5f281a30d263cf9c1c2c3068ade0a6f7984673f6
bjorn-nesby <bjorn.nesby@gmail.com>
2018-07-02 18:45:09 +0200

    - SSK: performance improvements for keyzone

--- Tools/com.renoise.SelectionShaper.xrnx/source/ssk_gui_keyzone.lua
+++ Tools/com.renoise.SelectionShaper.xrnx/source/ssk_gui_keyzone.lua
@@ -14,9 +14,9 @@ Simple multisample layout widget for SSK
 class 'SSK_Gui_Keyzone' (vControl)
 
 SSK_Gui_Keyzone.PALETTE = {
-  COLOR_BG = {0x00,0x00,0x00},      -- unassigned space 
-  COLOR_EMPTY = {0x1b,0x44,0x30},   -- sample without data
-  COLOR_CONTENT = {0x25,0x87,0x56}, -- sample with data 
+  COLOR_BG = {0x29,0x29,0x29},
+  COLOR_MAPPING = {0x1b,0x44,0x30},
+  COLOR_MAPPING_SELECTED = {0x25,0x87,0x56},
 }
 
 ---------------------------------------------------------------------------------------------------
@@ -25,41 +25,58 @@ function SSK_Gui_Keyzone:__init(...)
 
   local args = cLib.unpack_args(...)
 
+  -- layout properties ---------------------------
+
+  --- xKeyZoneLayout
+  self.layout = property(self.get_layout) 
+
   self.note_steps = property(self.get_note_steps,self.set_note_steps) 
-  self.note_steps_observable = renoise.Document.ObservableNumber(args.note_steps or SSK_Prefs.DEFAULT_NOTE_STEPS)
+  self.note_steps_observable = renoise.Document.ObservableNumber(
+    args.note_steps or xKeyZone.DEFAULT_NOTE_STEPS)
   -- 
   self.note_min = property(self.get_note_min,self.set_note_min) 
-  self.note_min_observable = renoise.Document.ObservableNumber(args.note_min or xSampleMapping.MIN_NOTE)
+  self.note_min_observable = renoise.Document.ObservableNumber(
+    args.note_min or xSampleMapping.MIN_NOTE)
   --
   self.note_max = property(self.get_note_max,self.set_note_max) 
-  self.note_max_observable = renoise.Document.ObservableNumber(args.note_max or xSampleMapping.MAX_NOTE)
+  self.note_max_observable = renoise.Document.ObservableNumber(
+    args.note_max or xSampleMapping.MAX_NOTE)
   --
   self.extend_notes = property(self.get_extend_notes,self.set_extend_notes)
-  self.extend_notes_observable = renoise.Document.ObservableBoolean(cReflection.as_boolean(args.extend_notes) or SSK_Prefs.DEFAULT_EXTEND_NOTES)
+  self.extend_notes_observable = renoise.Document.ObservableBoolean(
+    cReflection.as_boolean(args.extend_notes) or xKeyZone.DEFAULT_EXTEND_NOTES)
 
   self.vel_steps = property(self.get_vel_steps,self.set_vel_steps) 
-  self.vel_steps_observable = renoise.Document.ObservableNumber(args.vel_steps or xKeyZone.DEFAULT_VEL_STEPS)
+  self.vel_steps_observable = renoise.Document.ObservableNumber(
+    args.vel_steps or xKeyZone.DEFAULT_VEL_STEPS)
   --
   self.vel_min = property(self.get_vel_min,self.set_vel_min) 
-  self.vel_min_observable = renoise.Document.ObservableNumber(args.vel_min or xSampleMapping.MIN_VELOCITY)
+  self.vel_min_observable = renoise.Document.ObservableNumber(
+    args.vel_min or xSampleMapping.MIN_VELOCITY)
   --
   self.vel_max = property(self.get_vel_max,self.set_vel_max) 
-  self.vel_max_observable = renoise.Document.ObservableNumber(args.vel_max or xSampleMapping.MAX_VELOCITY)
+  self.vel_max_observable = renoise.Document.ObservableNumber(
+    args.vel_max or xSampleMapping.MAX_VELOCITY)
+
+  -- other properties -----------------
 
   self.color_bg = property(self.get_color_bg,self.set_color_bg) 
   self._color_bg = args.color_bg or SSK_Gui_Keyzone.PALETTE.COLOR_BG
 
-  self.color_empty = property(self.get_color_empty,self.set_color_empty) 
-  self._color_empty = args.color_empty or SSK_Gui_Keyzone.PALETTE.COLOR_EMPTY
+  self.color_mapping = property(self.get_color_mapping,self.set_color_mapping) 
+  self._color_mapping = args.color_mapping or SSK_Gui_Keyzone.PALETTE.COLOR_MAPPING
 
-  self.color_content = property(self.get_color_content,self.set_color_content) 
-  self._color_content = args.color_content or SSK_Gui_Keyzone.PALETTE.COLOR_CONTENT
+  self.color_selected = property(self.get_color_selected,self.set_color_selected) 
+  self._color_selected = args.color_selected or SSK_Gui_Keyzone.PALETTE.COLOR_MAPPING_SELECTED
+
+  -- function, @param (xSampleMapping)
+  self.pressed = args.pressed
 
   -- internal --
 
-
-  -- table<vSampleMapping>
+  -- table<xSampleMapping>
   self._mappings = {}
+
   -- table<vButtonStrip>
   self._rows = {}
   -- table {...} 
@@ -180,24 +197,38 @@ end
 
 ---------------------------------------------------------------------------------------------------
 
-function SSK_Gui_Keyzone:get_color_empty()
-  return self._color_empty
+function SSK_Gui_Keyzone:get_color_mapping()
+  return self._color_mapping
 end
 
-function SSK_Gui_Keyzone:set_color_empty(val)
-  self._color_empty = val
+function SSK_Gui_Keyzone:set_color_mapping(val)
+  self._color_mapping = val
   self:request_update()
 end
 
 ---------------------------------------------------------------------------------------------------
 
-function SSK_Gui_Keyzone:get_color_content()
-  return self._color_content
+function SSK_Gui_Keyzone:get_color_selected()
+  return self._color_selected
 end
 
-function SSK_Gui_Keyzone:set_color_content(val)
-  self._color_content = val
+function SSK_Gui_Keyzone:set_color_selected(val)
+  self._color_selected = val
   self:request_update()
+end
+
+---------------------------------------------------------------------------------------------------
+
+function SSK_Gui_Keyzone:get_layout()
+  return xKeyZoneLayout{
+    note_steps = self.note_steps,
+    note_min = self.note_min,
+    note_max = self.note_max,
+    vel_steps = self.vel_steps,
+    vel_min = self.vel_min,
+    vel_max = self.vel_max,
+    -- etc.
+  }
 end
 
 ---------------------------------------------------------------------------------------------------
@@ -218,15 +249,9 @@ function SSK_Gui_Keyzone:build()
 
   self:_clear()
 
-  -- compute layout
-  self._mappings = xKeyZone.create_multisample_layout(xKeyZoneLayout{
-    note_steps = self.note_steps,
-    note_min = self.note_min,
-    note_max = self.note_max,
-    vel_steps = self.vel_steps,
-    vel_min = self.vel_min,
-    vel_max = self.vel_max
-  })
+  -- create layout with the single purpose of identifying 
+  -- existing/matching samples (or lack thereof...)
+  self._mappings = xKeyZone.create_multisample_layout(self.layout,rns.selected_instrument)
 
   -- compute "rows" 
   self:_compute_velocities()
@@ -238,13 +263,17 @@ function SSK_Gui_Keyzone:build()
 
   -- create strips 
   for k,v in ipairs(self._row_data) do 
+
     local vstrip = vButtonStrip{
       vb = vb,
       width = self.width,
       pressed = function(idx,_strip_)
-        print(">>> pressed: ",k,idx,rprint(v),rprint(_strip_.items[idx]))
+        --print(">>> pressed: ",k,idx,rprint(v),_strip_.items[idx].value)
+        if (type(self.pressed)=="function") then 
+          self.pressed(_strip_.items[idx].value)
+        end
       end,
-      items = self:_compute_notes(v.velocity_range,v.void),
+      items = self:_compute_notes(v.velocity_range,v.void)
     }
     -- apply height afterwards, to retain dimensions
     vstrip.height = heights[k]
@@ -316,17 +345,50 @@ end
 ---------------------------------------------------------------------------------------------------
 -- populate each strip with members 
 
-function SSK_Gui_Keyzone:_compute_notes(velocity_range,void)
-  TRACE("SSK_Gui_Keyzone:_compute_notes(velocity_range,void)",velocity_range,void)
+function SSK_Gui_Keyzone:_compute_notes(vel_rng,void)
+  TRACE("SSK_Gui_Keyzone:_compute_notes(vel_rng,void)",vel_rng,void)
 
   local rslt = {}
 
+  local rns_mapping = rns.selected_sample and rns.selected_sample.sample_mapping 
+  -- compute notes without extend to obtain basenote
+  local basenotes = xKeyZone.compute_multisample_notes(
+    self.note_steps,self.note_min,self.note_max,false)
   local notes = xKeyZone.compute_multisample_notes(
     self.note_steps,self.note_min,self.note_max,self.extend_notes)
   for k,v in ipairs(notes) do 
+    -- check if mapping matches selection in renoise 
+    local is_selected = false
+    if rns_mapping then     
+      if (cLib.table_compare(rns_mapping.note_range,v) 
+        and cLib.table_compare(rns_mapping.velocity_range,vel_rng))
+      then
+        is_selected = true
+      end
+    end
+    -- match against samples in instrument (_mappings property)
+    local buffer = nil
+    local mapping = xKeyZone.find_mapping(self._mappings,v,vel_rng)
+    if mapping and mapping.sample then 
+      buffer = xSample.get_sample_buffer(mapping.sample)
+    end
+
+    local str_base = xNoteColumn.note_value_to_string(basenotes[k][1])
+    local str_nmin = xNoteColumn.note_value_to_string(v[1])
+    local str_nmax = xNoteColumn.note_value_to_string(v[2])
+    local str_vmin = ("%.2X"):format(vel_rng[1])
+    local str_vmax = ("%.2X"):format(vel_rng[2])
     table.insert(rslt,vButtonStripMember{
       weight = v[2]-v[1],
-      color = not void and self.color_content or self.color_empty,
+      color = not void and is_selected and self.color_selected 
+        or buffer and self.color_mapping or self.color_bg,
+      tooltip = ("Base Note: %s\nNote Range: (%s - %s)\nVelocity Range: (%s - %s)"):format(
+        str_base,str_nmin,str_nmax,str_vmin,str_vmax),
+      value = xSampleMapping{
+        base_note = basenotes[k][1],
+        note_range = {v[1],v[2]},
+        velocity_range = vel_rng
+      },
     })
   end
 
